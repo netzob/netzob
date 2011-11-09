c54cdb10604ced7aa4e32edcda2a882042a2a745
Olivier Tétard <olivier.tetard@amossys.fr>
2011-11-09 17:27:37 +0000

    setup.py: install icons in the correct folders

--- setup.py
+++ setup.py
@@ -69,7 +69,10 @@ setup(
     package_dir = {"netzob": "src/netzob" },
     ext_modules=[moduleLibNeedleman],
     data_files = [
-        ('share/netzob', ['resources/static/logo.png'])
+        ('share/netzob', ['resources/static/logo.png']),
+        ('share/icons/hicolor/22x22/apps/', ["resources/static/icons/22x22/netzob.png"]),
+        ('share/icons/hicolor/48x48/apps/', ["resources/static/icons/48x48/netzob.png"]),
+        ('share/icons/hicolor/64x64/apps/', ["resources/static/icons/64x64/netzob.png"])
         ],
     scripts = ["netzob"],
     version="0.3.0",
