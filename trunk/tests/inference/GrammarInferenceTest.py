9a959dd411d292f8e551fd7bd609acbc918085c8
Georges Bossert <georges.bossert@supelec.fr>
2011-11-21 15:56:52 +0000

    A test which verifies the active infering process.

--- trunk/tests/inference/GrammarInferenceTest.py
+++ trunk/tests/inference/GrammarInferenceTest.py
@@ -18,8 +18,10 @@
 #| Standard library imports
 #+---------------------------------------------------------------------------+
 import unittest
-
+import random
+import os
 import time
+import logging
 
 #+---------------------------------------------------------------------------+
 #| Related third party imports
@@ -31,6 +33,12 @@ from xml.etree import ElementTree
 #+---------------------------------------------------------------------------+
 from netzob.Inference.Grammar.Oracles.NetworkOracle import NetworkOracle
 from netzob.Inference.Grammar.GrammarInferer import GrammarInferer
+from netzob.Common.MMSTD.Actors.Network.NetworkClient import NetworkClient
+from netzob.Common.MMSTD.Actors.Network.NetworkServer import NetworkServer
+from netzob.Common.MMSTD.Actors.MMSTDVisitor import MMSTDVisitor
+from netzob.Common.MMSTD.Dictionary.AbstractionLayer import AbstractionLayer
+from netzob.Common.ConfigurationParser import ConfigurationParser
+from netzob.Common.MMSTD.Tools.Parsers.MMSTDParser.MMSTDXmlParser import MMSTDXmlParser
 
 class GrammarInferenceTest(unittest.TestCase):
     
@@ -39,11 +47,47 @@ class GrammarInferenceTest(unittest.TestCase):
     
     def test_grammarInference(self):
         
-        dictionary = None
+        # First we create the server and starts it
+        actorGrammar = "example_learning.xml"
+        actorIP = "localhost"
+        actorPort = random.randint(9000, 9999)
+        actorNetworkProtocol = "TCP"
+        
+        serverName = "Server"
+        
+        
+        # Create a network server
+        grammar_directory = ConfigurationParser().get("automata", "path") 
+        xmlFile = os.path.join(grammar_directory, actorGrammar)
+        tree = ElementTree.ElementTree()
+        tree.parse(xmlFile)
+        # Load the automata based on its XML definition
+        serverAutomata = MMSTDXmlParser.loadFromXML(tree.getroot())
+        dictionary = serverAutomata.getDictionary()
+            
+        # Create the network layer
+        serverCommunicationChannel = NetworkServer(actorIP, actorNetworkProtocol, actorPort)
+        
+        # Create the abstraction layer for this connection
+        serverAbstractionLayer = AbstractionLayer(serverCommunicationChannel, serverAutomata.getDictionary())
+        
+        # And we create an MMSTD visitor for this
+        server = MMSTDVisitor(serverName, serverAutomata, False, serverAbstractionLayer) 
+        server.run()     
+        
+        logging.info("Starting the server")
+        time.sleep(3)
+        
+        
+        
         
         # Lets create a simple network oracle
-        oracle = NetworkOracle()
+        oracleCommunicationChannel = NetworkClient(actorIP, actorNetworkProtocol, actorPort)
+        oracle = NetworkOracle(oracleCommunicationChannel)
         equivalenceOracle = None
         
         inferer = GrammarInferer(dictionary, oracle, equivalenceOracle)
         inferer.infer()
+
+        server.stop()
+
