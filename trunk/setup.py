b15755d16c6d38287f05871461676d2a8942109c
Georges Bossert <georges.bossert@supelec.fr>
2011-12-20 16:36:10 +0000

    Source code header for main file

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,16 +2,28 @@
 # -*- coding: utf-8 -*-
 
 #+---------------------------------------------------------------------------+
-#|         01001110 01100101 01110100 01111010 01101111 01100010             | 
+#|          01001110 01100101 01110100 01111010 01101111 01100010            |
+#|                                                                           |
+#|               Netzob : Inferring communication protocols                  |
 #+---------------------------------------------------------------------------+
-#| NETwork protocol modeliZatiOn By reverse engineering                      |
-#| ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
-#| @license      : GNU GPL v3                                                |
-#| @copyright    : Georges Bossert and Frederic Guihery                      |
-#| @url          : http://code.google.com/p/netzob/                          |
-#| ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
-#| @author       : {gbt,fgy}@amossys.fr                                      |
-#| @organization : Amossys, http://www.amossys.fr                            |
+#| Copyright (C) 2011 Georges Bossert and Frédéric Guihéry                   |
+#| This program is free software: you can redistribute it and/or modify      |
+#| it under the terms of the GNU General Public License as published by      |
+#| the Free Software Foundation, either version 3 of the License, or         |
+#| (at your option) any later version.                                       |
+#|                                                                           |
+#| This program is distributed in the hope that it will be useful,           |
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
+#| GNU General Public License for more details.                              |
+#|                                                                           |
+#| You should have received a copy of the GNU General Public License         |
+#| along with this program. If not, see <http://www.gnu.org/licenses/>.      |
+#+---------------------------------------------------------------------------+
+#| @url      : http://www.netzob.org                                         |
+#| @contact  : contact@netzob.org                                            |
+#| @sponsors : Amossys, http://www.amossys.fr                                |
+#|             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
 #+---------------------------------------------------------------------------+
 
 #+----------------------------------------------------------------------------
