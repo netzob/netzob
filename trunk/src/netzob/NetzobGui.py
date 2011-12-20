f4cd4359791b8ae58b0ed1622b295f457facb00a
Georges Bossert <georges.bossert@supelec.fr>
2011-12-20 08:36:42 +0000

    Update the header comment to include the full version of the GPLv3 copyright notice

--- trunk/src/netzob/NetzobGui.py
+++ trunk/src/netzob/NetzobGui.py
@@ -2,18 +2,29 @@
 # -*- coding: utf-8 -*-
 
 #+---------------------------------------------------------------------------+
-#|         01001110 01100101 01110100 01111010 01101111 01100010             | 
+#|          01001110 01100101 01110100 01111010 01101111 01100010            |
+#|                                                                           |
+#|               Netzob : Inferring communication protocols                  |
 #+---------------------------------------------------------------------------+
-#| Netzob : communication protocol modelization by reverse engineering                     |
-#| ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
-#| @license      : GNU GPL v3                                                |
-#| @copyright    : Georges Bossert and Frédéric Guihéry                      |
-#| @url          : http://www.netzob.org                                     |
-#| ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
-#| @contact      : {gbt,fgy}@amossys.fr                                      |
-#| @organization : Amossys, http://www.amossys.fr                            |
-#|                 Supelec, http://www.rennes.supelec.fr/ren/rd/ssir/        |
+#| Copyright (C) 2011 Georges Bossert and Frédéric Guihéry                   |
+#| This program is free software: you can redistribute it and/or modify      |
+#| it under the terms of the GNU General Public License as published by      |
+#| the Free Software Foundation, either version 3 of the License, or         |
+#| (at your option) any later version.                                       |
+#|                                                                           |
+#| This program is distributed in the hope that it will be useful,           |
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
+#| GNU General Public License for more details.                              |
+#|                                                                           |
+#| You should have received a copy of the GNU General Public License         |
+#| along with this program. If not, see <http://www.gnu.org/licenses/>.      |
 #+---------------------------------------------------------------------------+
+#| @url      : http://www.netzob.org                                         |
+#| @contact  : contact@netzob.org                                            |
+#| @sponsors : Amossys, http://www.amossys.fr                                |
+#|             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
+#+---------------------------------------------------------------------------+ 
 
 #+---------------------------------------------------------------------------+ 
 #| Standard library imports
@@ -176,7 +187,11 @@ class NetzobGui():
         # it means we simulate a save and compare the XML with the current one
         if self.getCurrentProject() != None and self.getCurrentProject().hasPendingModifications(self.getCurrentWorkspace()) :
             self.offerToSaveCurrentProject()
-        
+        # We also save the workspace
+        try :
+            self.getCurrentWorkspace().saveConfigFile()
+        except :
+            pass
         
         for page in self.pageList:
             page[1].kill()
