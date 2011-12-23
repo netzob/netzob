c36fd6bb08499ae7e8b8ee9d5753ba9e2f210d33
Georges Bossert <georges.bossert@supelec.fr>
2011-12-23 19:55:04 +0100

    Introduces the notion of Task to describe a "search" operation which includes all its variations.

---
+++ src/netzob/Inference/Vocabulary/SearchTask.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+#+---------------------------------------------------------------------------+
+#|          01001110 01100101 01110100 01111010 01101111 01100010            |
+#|                                                                           |
+#|               Netzob : Inferring communication protocols                  |
+#+---------------------------------------------------------------------------+
+#| Copyright (C) 2011 Georges Bossert and Frédéric Guihéry                   |
+#| This program is free software: you can redistribute it and/or modify      |
+#| it under the terms of the GNU General Public License as published by      |
+#| the Free Software Foundation, either version 3 of the License, or         |
+#| (at your option) any later version.                                       |
+#|                                                                           |
+#| This program is distributed in the hope that it will be useful,           |
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
+#| GNU General Public License for more details.                              |
+#|                                                                           |
+#| You should have received a copy of the GNU General Public License         |
+#| along with this program. If not, see <http://www.gnu.org/licenses/>.      |
+#+---------------------------------------------------------------------------+
+#| @url      : http://www.netzob.org                                         |
+#| @contact  : contact@netzob.org                                            |
+#| @sponsors : Amossys, http://www.amossys.fr                                |
+#|             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
+#+---------------------------------------------------------------------------+
+
+#+---------------------------------------------- 
+#| Global Imports
+#+----------------------------------------------
+import logging
+
+
+#+---------------------------------------------- 
+#| Local Imports
+#+----------------------------------------------
+
+#+---------------------------------------------- 
+#| SearchTask :
+#|     Describes a search operation
+#| @author     : {gbt,fgy}@amossys.fr
+#| @version    : 0.2
+#+---------------------------------------------- 
+class SearchTask(object):
+    
+    #+---------------------------------------------- 
+    #| Constructor :
+    #| @param data: the searched data
+    #| @param type: the type of the searched data
+    #+----------------------------------------------   
+    def __init__(self, data, type):
+        # create logger with the given configuration
+        self.log = logging.getLogger('netzob.Inference.Vocabulary.SearchTask.py')
+        self.data = data
+        self.type = type
+        self.searchedDatas = dict()
+    
+    def registerVariation(self, data, description):
+        self.searchedDatas[data] = description
+
+        
