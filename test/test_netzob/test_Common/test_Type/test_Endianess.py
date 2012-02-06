98ed63d5dd505ae9e933c6f1446c23238b80b3d0
Georges Bossert <georges.bossert@supelec.fr>
2012-02-06 22:30:19 +0100

    Adding two unit tests for netzob.Common.Type.Endianess

---
+++ test/test_netzob/test_Common/test_Type/test_Endianess.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+#+---------------------------------------------------------------------------+
+#|          01001110 01100101 01110100 01111010 01101111 01100010            |
+#|                                                                           |
+#|               Netzob : Inferring communication protocols                  |
+#+---------------------------------------------------------------------------+
+#| Copyright (C) 2011 Georges Bossert and Frédéric Guihéry                   |
+#| This program is free software: you can redistribute it and/or modify      |
+#| it under the terms of the GNU General Public License as published by      |
+#| the Free Software Foundation, either version 3 of the License, or         |
+#| (at your option) any later version.                                       |
+#|                                                                           |
+#| This program is distributed in the hope that it will be useful,           |
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
+#| GNU General Public License for more details.                              |
+#|                                                                           |
+#| You should have received a copy of the GNU General Public License         |
+#| along with this program. If not, see <http://www.gnu.org/licenses/>.      |
+#+---------------------------------------------------------------------------+
+#| @url      : http://www.netzob.org                                         |
+#| @contact  : contact@netzob.org                                            |
+#| @sponsors : Amossys, http://www.amossys.fr                                |
+#|             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
+#+---------------------------------------------------------------------------+
+
+import unittest
+from netzob.Common.Type.Endianess import Endianess
+
+class tests_Endianess(unittest.TestCase):
+
+    def test_BIG(self):
+        self.assertEqual(Endianess.BIG, "big-endian")
+
+    def test_LITTLE(self):
+        self.assertEqual(Endianess.LITTLE, "little-endian")
+
