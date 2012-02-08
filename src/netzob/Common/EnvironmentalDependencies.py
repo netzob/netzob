a599b5749ab5053b4fe706e072745196700d3671
Maxime Olivier <maxime@miskin.fr>
2012-02-08 17:12:45 +0100

    Fix pep E231 missing whitespace after ','

--- src/netzob/Common/EnvironmentalDependencies.py
+++ src/netzob/Common/EnvironmentalDependencies.py
@@ -78,7 +78,7 @@ class EnvironmentalDependencies(object):
         # Trick to retrieve the usual IP address
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.connect(("gmail.com",80))
+            s.connect(("gmail.com", 80))
             ip_address = s.getsockname()[0]
             s.close()
         except:
