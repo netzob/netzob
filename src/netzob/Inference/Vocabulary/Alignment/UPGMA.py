f8449f10c51d518fd2ec8990c964f58a264057f9
Georges Bossert <georges.bossert@supelec.fr>
2012-02-22 17:20:04 +0100

    UPGMA dedicated class including first unit tests

---
+++ src/netzob/Inference/Vocabulary/Alignment/UPGMA.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+#+---------------------------------------------------------------------------+
+#|          01001110 01100101 01110100 01111010 01101111 01100010            |
+#|                                                                           |
+#|               Netzob : Inferring communication protocols                  |
+#+---------------------------------------------------------------------------+
+#| Copyright (C) 2011 Georges Bossert and Frédéric Guihéry                   |
+#| This program is free software: you can redistribute it and/or modify      |
+#| it under the terms of the GNU General Public License as published by      |
+#| the Free Software Foundation, either version 3 of the License, or         |
+#| (at your option) any later version.                                       |
+#|                                                                           |
+#| This program is distributed in the hope that it will be useful,           |
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
+#| GNU General Public License for more details.                              |
+#|                                                                           |
+#| You should have received a copy of the GNU General Public License         |
+#| along with this program. If not, see <http://www.gnu.org/licenses/>.      |
+#+---------------------------------------------------------------------------+
+#| @url      : http://www.netzob.org                                         |
+#| @contact  : contact@netzob.org                                            |
+#| @sponsors : Amossys, http://www.amossys.fr                                |
+#|             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
+#+---------------------------------------------------------------------------+
+
+#+---------------------------------------------------------------------------+
+#| Global Imports
+#+---------------------------------------------------------------------------+
+import logging
+
+#+---------------------------------------------------------------------------+
+#| Local Imports
+#+---------------------------------------------------------------------------+
+from netzob.Common.Type.TypeConvertor import TypeConvertor
+
+#+---------------------------------------------------------------------------+
+#| C Imports
+#+---------------------------------------------------------------------------+
+import libNeedleman
+from netzob.Common.Symbol import Symbol
+import uuid
+
+#+---------------------------------------------------------------------------+
+#| UPGMA:
+#|     Supports the use of UPGMA clustering
+#+---------------------------------------------------------------------------+
+class UPGMA(object):
+    
+    def __init__(self, symbols, nbIteration, minEquivalence, doInternalSlick, cb_status=None):
+        self.symbols = symbols
+        self.nbIteration = nbIteration
+        self.minEquivalence = minEquivalence
+        self.doInternalSlick = doInternalSlick
+        self.cb_status = cb_status
+        self.log = logging.getLogger('netzob.Inference.Vocabulary.UPGMA.py')
+    
+    #+-----------------------------------------------------------------------+
+    #| cb_executionStatus
+    #|     Callback function called by the C extension to provide info on status
+    #| @param donePercent a float between 0 and 100 included
+    #| @param currentMessage a str which represents the current alignment status
+    #+-----------------------------------------------------------------------+
+    def cb_executionStatus(self, donePercent, currentMessage):
+        if self.cb_status == None :
+            print "[UPGMA status] " + str(donePercent) + "% " + currentMessage
+        else :
+            self.cb_status(donePercent, currentMessage)
+    
+    #+-----------------------------------------------------------------------+
+    #| executeClustering
+    #|     execute the clustering operation
+    #| @param symbols the list of symbol to consider in the clustering
+    #| @nbIteration the number of iteration maximum
+    #| @minEquivalence the minimum requirement to consider two symbol as equivalent
+    #| @return the new list of symbols
+    #+-----------------------------------------------------------------------+
+    def executeClustering(self):    
+        self.log.debug("Re-Organize the symbols (nbIteration={0}, min_equivalence={1})".format(self.nbIteration, self.minEquivalence))
+        for iteration in range(0, self.nbIteration):
+            self.log.debug("Iteration {0} started...".format(str(iteration)))
+            # Create the score matrix for each symbol
+            (i_maximum, j_maximum, maximum) = self.retrieveEffectiveMaxIJ()
+
+            self.log.debug("Searching for the maximum of equivalence.")
+            if (maximum >= self.minEquivalence):
+                self.log.info("Merge the column/line {0} with the column/line {1} ; score = {2}".format(str(i_maximum), str(j_maximum), str(maximum)))
+                self.mergeEffectiveRowCol(i_maximum, j_maximum)
+            else:
+                self.log.info("Stopping the clustering operation since the maximum found is {0} (<{1})".format(str(maximum), str(self.minEquivalence)))
+                break
+
+        # Compute the regex/alignment of each symbol
+        for symbol in self.symbols:
+            symbol.buildRegexAndAlignment(self.configuration)
+
+            
+    #+----------------------------------------------
+    #| retrieveMaxIJ:
+    #|   given a list of symbols, it computes the
+    #|   the possible two symbols which can be merged
+    #| @return (i,j,max) (i,j) path in the matrix of
+    #|                   the symbols to merge
+    #|                    max score of the two symbols
+    #+----------------------------------------------
+    def retrieveEffectiveMaxIJ(self):
+        self.log.debug("Computing the associated matrix")
+        
+        # Serialize the symbols
+        (serialSymbols, formatSymbols) = TypeConvertor.serializeSymbols(self.symbols)
+        
+        # Execute the Clustering part in C :) (thx fgy)
+        debug = True
+        (i_max, j_max, maxScore) = libNeedleman.getHighestEquivalentGroup(self.doInternalSlick, len(self.symbols), formatSymbols, serialSymbols, self.cb_executionStatus, debug)
+        return (i_max, j_max, maxScore)
+    
+    
+    #+----------------------------------------------
+    #| mergeRowCol:
+    #|   Merge the symbols i and j in the "symbols" structure
+    #+----------------------------------------------
+    def mergeEffectiveRowCol(self, i_maximum, j_maximum):
+        # Extract symbols i and j
+        if i_maximum > j_maximum:
+            symbol1 = self.symbols.pop(i_maximum)
+            symbol2 = self.symbols.pop(j_maximum)
+        else:
+            symbol1 = self.symbols.pop(j_maximum)
+            symbol2 = self.symbols.pop(i_maximum)
+
+        # Merge the symbols i and j
+        messages = []
+        messages.extend(symbol1.getMessages())
+        messages.extend(symbol2.getMessages())
+
+        newSymbol = Symbol(str(uuid.uuid4()), "Symbol", self.project)
+        for message in messages:
+            newSymbol.addMessage(message)
+
+        # Append th new symbol to the "symbols" structure
+        self.symbols.append(newSymbol)  
+        
+        
+    #+-----------------------------------------------------------------------+
+    #| deserializeGroups
+    #|     Useless (functionally) function created for testing purposes
+    #| @param symbols a list of symbols
+    #| @returns number Of Deserialized symbols
+    #+-----------------------------------------------------------------------+
+    def deserializeMessages(self, symbols): 
+        # First we serialize the messages
+        (serialSymbols, format) = TypeConvertor.serializeSymbols(symbols)
+        
+        debug = True
+        return libNeedleman.deserializeGroups(len(symbols), format, serialSymbols, debug)
+        
---
+++ test/src/test_netzob/test_Alignment/test_UPGMAInC.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+#+---------------------------------------------------------------------------+
+#|          01001110 01100101 01110100 01111010 01101111 01100010            |
+#|                                                                           |
+#|               Netzob : Inferring communication protocols                  |
+#+---------------------------------------------------------------------------+
+#| Copyright (C) 2011 Georges Bossert and Frédéric Guihéry                   |
+#| This program is free software: you can redistribute it and/or modify      |
+#| it under the terms of the GNU General Public License as published by      |
+#| the Free Software Foundation, either version 3 of the License, or         |
+#| (at your option) any later version.                                       |
+#|                                                                           |
+#| This program is distributed in the hope that it will be useful,           |
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
+#| GNU General Public License for more details.                              |
+#|                                                                           |
+#| You should have received a copy of the GNU General Public License         |
+#| along with this program. If not, see <http://www.gnu.org/licenses/>.      |
+#+---------------------------------------------------------------------------+
+#| @url      : http://www.netzob.org                                         |
+#| @contact  : contact@netzob.org                                            |
+#| @sponsors : Amossys, http://www.amossys.fr                                |
+#|             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
+#+---------------------------------------------------------------------------+
+
+#+---------------------------------------------------------------------------+ 
+#| Standard library imports
+#+---------------------------------------------------------------------------+
+import uuid
+import time
+import random
+import string
+import unittest
+
+from netzob.Common.ExecutionContext import ExecutionContext
+from netzob.Common.Models.RawMessage import RawMessage
+from netzob.Common.Symbol import Symbol
+from netzob.Common.Type.TypeConvertor import TypeConvertor
+from netzob.Inference.Vocabulary.Alignment.UPGMA import UPGMA
+
+#+---------------------------------------------------------------------------+
+#| Local Imports
+#+---------------------------------------------------------------------------+
+
+
+class test_UPGMAInC(unittest.TestCase):
+    
+    def generateRandomString(self, min_len, max_len):
+        return ''.join((random.choice(string.letters + string.digits) for _ in xrange(random.randint(min_len, max_len))))
+    
+    def test_deserialisationGroups(self):        
+        symbols = []
+        nbSymbol = random.randint(2, 50)
+        for iSymbol in range(0, nbSymbol) :        
+            # We create 6 messages of 2 group
+            originalSymbol = Symbol(uuid.uuid4(), "TestSymbol", None)        
+            # group1 
+            message1 = RawMessage(uuid.uuid4(), str(time.time()), TypeConvertor.stringToNetzobRaw("bonjour " + self.generateRandomString(20, 30) + " comment vas-tu ?"))
+            message2 = RawMessage(uuid.uuid4(), str(time.time()), TypeConvertor.stringToNetzobRaw("bonjour " + self.generateRandomString(20, 30) + " comment vas-tu ?"))
+            message3 = RawMessage(uuid.uuid4(), str(time.time()), TypeConvertor.stringToNetzobRaw("bonjour " + self.generateRandomString(20, 30) + " comment vas-tu ?"))
+            # group2
+            message4 = RawMessage(uuid.uuid4(), str(time.time()), TypeConvertor.stringToNetzobRaw("salut à toi " + self.generateRandomString(10, 15) + " what's up ?"))
+            message5 = RawMessage(uuid.uuid4(), str(time.time()), TypeConvertor.stringToNetzobRaw("salut à toi " + self.generateRandomString(10, 15) + " what's up ?"))
+            message6 = RawMessage(uuid.uuid4(), str(time.time()), TypeConvertor.stringToNetzobRaw("salut à toi " + self.generateRandomString(10, 15) + " what's up ?"))
+            
+            originalSymbol.addMessage(message1)
+            originalSymbol.addMessage(message2)
+            originalSymbol.addMessage(message3)
+            originalSymbol.addMessage(message4)
+            originalSymbol.addMessage(message5)
+            originalSymbol.addMessage(message6)
+            symbols.append(originalSymbol)
+            
+            
+        # Start the clustering
+        clusteringSolution = UPGMA([originalSymbol], 100, 90, True)
+        result = clusteringSolution.deserializeMessages(symbols)
+        self.assertEqual(result, len(symbols))
+        
+        
