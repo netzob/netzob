480bb9452d994c115ea3301f38557c6c9fd3d0cd
Georges Bossert <georges.bossert@supelec.fr>
2012-03-14 19:18:38 +0100

    Escape html operators in the name of a colum

--- src/netzob/Inference/Vocabulary/TreeViews/TreeMessageGenerator.py
+++ src/netzob/Inference/Vocabulary/TreeViews/TreeMessageGenerator.py
@@ -32,6 +32,7 @@ import logging
 import pango
 import gobject
 import gtk
+import glib
 
 #+----------------------------------------------
 #| Local Imports
@@ -190,7 +191,7 @@ class TreeMessageGenerator():
         regex_row.append(pango.WEIGHT_BOLD)
         regex_row.append(True)
         for field in self.symbol.getFields():
-            regex_row.append(field.getEncodedVersionOfTheRegex())
+            regex_row.append(glib.markup_escape_text(field.getEncodedVersionOfTheRegex()))
         
         # Build the types row
         types_line = []
@@ -206,12 +207,12 @@ class TreeMessageGenerator():
         for line in content_lines :
             self.treestore.append(None, line)
                     
-        # activate or deactiave the perfect number of columns = nb Field
+        # activate or deactivate the perfect number of columns = nb Field
         for col in self.treeview.get_columns():
             self.treeview.remove_column(col)
         for i in range(0, min(200, len(self.symbol.getFields()))) :
             self.treeview.append_column(self.currentColumns[i])
-            self.treeview.get_column(i).set_title( self.symbol.getFieldByIndex(i).getName() )
+            self.treeview.get_column(i).set_title(self.symbol.getFieldByIndex(i).getName())
 
         self.treeview.set_model(self.treestore)
 
