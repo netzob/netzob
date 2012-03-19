38d262c2907d80a51da34102286045c6f411c5be
Georges Bossert <georges.bossert@supelec.fr>
2012-03-19 19:08:45 +0100

    Update the form of automatic grammar inference to support infering clients (see #137)

--- src/netzob/Inference/Grammar/AutomaticGrammarInferenceView.py
+++ src/netzob/Inference/Grammar/AutomaticGrammarInferenceView.py
@@ -38,6 +38,7 @@ from netzob.Simulator.XDotWidget import XDotWidget
 import gobject
 from netzob.Common.Threads.Tasks.ThreadedTask import ThreadedTask
 from netzob.Common.Threads.Job import Job
+from netzob.Common.MMSTD.Actors.Network.NetworkServer import NetworkServer
 
 pygtk.require('2.0')
 
@@ -67,15 +68,31 @@ class AutomaticGrammarInferenceView(object):
         # Display the form for the creation of a word variable
         self.dialog = gtk.Dialog(title="Configuration of the automatic inference", flags=0, buttons=None)
 
-        mainTable = gtk.Table(rows=8, columns=2, homogeneous=False)
+        mainTable = gtk.Table(rows=9, columns=2, homogeneous=False)
+        
+        
+        # Infer a client or a server
+        targetOfInferenceLabel = gtk.Label("Target : ")
+        targetOfInferenceLabel.show()
+        self.targetOfInferenceCombo = gtk.ComboBox()
+        self.targetOfInferenceCombo.set_model(gtk.ListStore(str))
+        targetOfInferenceComboCell = gtk.CellRendererText()
+        self.targetOfInferenceCombo.pack_start(targetOfInferenceComboCell, True)
+        self.targetOfInferenceCombo.add_attribute(targetOfInferenceComboCell, 'text', 0)
+        self.targetOfInferenceCombo.append_text("CLIENT")
+        self.targetOfInferenceCombo.append_text("SERVER")
+        self.targetOfInferenceCombo.show()
+        mainTable.attach(targetOfInferenceLabel, 0, 1, 0, 1, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(self.targetOfInferenceCombo, 1, 2, 0, 1, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        
 
         # IP of the server
-        IPLabel = gtk.Label("IP of the server :")
+        IPLabel = gtk.Label("IP :")
         IPLabel.show()
         self.IPEntry = gtk.Entry()
         self.IPEntry.show()
-        mainTable.attach(IPLabel, 0, 1, 0, 1, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
-        mainTable.attach(self.IPEntry, 1, 2, 0, 1, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(IPLabel, 0, 1, 1, 2, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(self.IPEntry, 1, 2, 1, 2, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
 
         # Protocol of the server
         ProtocolLabel = gtk.Label("Protocol :")
@@ -87,46 +104,46 @@ class AutomaticGrammarInferenceView(object):
         self.combo_protocolOfNetworkActor.set_active(0)
         self.combo_protocolOfNetworkActor.show()
 
-        mainTable.attach(ProtocolLabel, 0, 1, 1, 2, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
-        mainTable.attach(self.combo_protocolOfNetworkActor, 1, 2, 1, 2, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(ProtocolLabel, 0, 1, 2, 3, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(self.combo_protocolOfNetworkActor, 1, 2, 2, 3, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
 
         # Source port
-        SourcePortLabel = gtk.Label("Source port :")
+        SourcePortLabel = gtk.Label("Our port :")
         SourcePortLabel.show()
-        self.SourcePortEntry = gtk.Entry()
-        self.SourcePortEntry.show()
-        mainTable.attach(SourcePortLabel, 0, 1, 2, 3, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
-        mainTable.attach(self.SourcePortEntry, 1, 2, 2, 3, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        self.ourPortEntry = gtk.Entry()
+        self.ourPortEntry.show()
+        mainTable.attach(SourcePortLabel, 0, 1, 3, 4, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(self.ourPortEntry, 1, 2, 3, 4, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
 
         # Port of the server
-        PortLabel = gtk.Label("Port :")
+        PortLabel = gtk.Label("Target Port :")
         PortLabel.show()
-        self.PortEntry = gtk.Entry()
-        self.PortEntry.show()
-        mainTable.attach(PortLabel, 0, 1, 3, 4, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
-        mainTable.attach(self.PortEntry, 1, 2, 3, 4, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        self.targetPortEntry = gtk.Entry()
+        self.targetPortEntry.show()
+        mainTable.attach(PortLabel, 0, 1, 4, 5, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(self.targetPortEntry, 1, 2, 4, 5, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
 
         # Estimated number of states
         MaxStatesLabel = gtk.Label("Maximum number of states :")
         MaxStatesLabel.show()
         self.MaxStatesEntry = gtk.Entry()
         self.MaxStatesEntry.show()
-        mainTable.attach(MaxStatesLabel, 0, 1, 4, 5, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
-        mainTable.attach(self.MaxStatesEntry, 1, 2, 4, 5, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(MaxStatesLabel, 0, 1, 5, 6, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(self.MaxStatesEntry, 1, 2, 5, 6, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
 
         # Script to execute to reset the implementation
         scriptLabel = gtk.Label("Reseting script :")
         scriptLabel.show()
         self.scriptEntry = gtk.Entry()
         self.scriptEntry.show()
-        mainTable.attach(scriptLabel, 0, 1, 5, 6, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
-        mainTable.attach(self.scriptEntry, 1, 2, 5, 6, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(scriptLabel, 0, 1, 6, 7, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(self.scriptEntry, 1, 2, 6, 7, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
 
         # Create button
         startButton = gtk.Button("Start inference")
         startButton.show()
         startButton.connect("clicked", self.startInference)
-        mainTable.attach(startButton, 1, 2, 6, 7, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
+        mainTable.attach(startButton, 1, 2, 7, 8, xoptions=gtk.FILL, yoptions=0, xpadding=5, ypadding=5)
 
         self.dialog.vbox.pack_end(mainTable, True, True, 0)
         self.dialog.show_all()
@@ -244,18 +261,25 @@ class AutomaticGrammarInferenceView(object):
 
     def startInference(self, button):
         # We retrieve the specified value
+        actorType = self.targetOfInferenceCombo.get_active_text()
         actorIP = self.IPEntry.get_text()
         actorNetworkProtocol = self.combo_protocolOfNetworkActor.get_active_text()
-        sourceActorPort = int(self.SourcePortEntry.get_text())
-        actorPort = int(self.PortEntry.get_text())
+        ourPort = int(self.ourPortEntry.get_text())
+        targetPort = int(self.targetPortEntry.get_text())
         scriptFilename = self.scriptEntry.get_text()
         maxNumberOfState = int(self.MaxStatesEntry.get_text())
 
         # Close the current dialog
         self.dialog.destroy()
 
-        # Lets create a simple network oracle
-        oracleCommunicationChannel = NetworkClient(actorIP, actorNetworkProtocol, actorPort, sourceActorPort)
+        if actorType == "CLIENT" :
+            # Lets create a simple network oracle
+            oracleCommunicationChannel = NetworkServer(actorIP, actorNetworkProtocol, targetPort, ourPort)
+        else :
+            # Lets create a simple network oracle
+            oracleCommunicationChannel = NetworkClient(actorIP, actorNetworkProtocol, targetPort, ourPort)
+            
+                
         # Lets create an equivalence oracle
         equivalenceOracle = WMethodNetworkEquivalenceOracle(oracleCommunicationChannel, maxNumberOfState)
 
