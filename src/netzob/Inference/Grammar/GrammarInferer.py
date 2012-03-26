33e9029b8fc1739913aa1576de13b67ddb909672
Georges Bossert <georges.bossert@supelec.fr>
2012-03-26 10:06:05 +0200

    Provide the reset script to WMethod

--- src/netzob/Inference/Grammar/GrammarInferer.py
+++ src/netzob/Inference/Grammar/GrammarInferer.py
@@ -50,11 +50,12 @@ import time
 #+----------------------------------------------
 class GrammarInferer(threading.Thread):
 
-    def __init__(self, vocabulary, oracle, equivalenceOracle, resetScript, cb_submitedQuery, cb_hypotheticalAutomaton):
+    def __init__(self, vocabulary, inputDictionary, oracle, equivalenceOracle, resetScript, cb_submitedQuery, cb_hypotheticalAutomaton):
         threading.Thread.__init__(self)
         # create logger with the given configuration
         self.log = logging.getLogger('netzob.Inference.Grammar.GrammarInferer.py')
         self.vocabulary = vocabulary
+        self.inputDictionary = inputDictionary
         self.oracle = oracle
         self.equivalenceOracle = equivalenceOracle
         self.resetScript = resetScript
@@ -97,7 +98,7 @@ class GrammarInferer(threading.Thread):
         
         
         # we first initialize the angluin's algo
-        self.learner = Angluin(self.vocabulary, self.oracle, self.resetScript, self.cb_submitedQuery, self.cb_hypotheticalAutomaton)
+        self.learner = Angluin(self.vocabulary, self.inputDictionary, self.oracle, self.resetScript, self.cb_submitedQuery, self.cb_hypotheticalAutomaton)
         while not equivalent and self.active:
             self.log.info("=============================================================================")
             self.log.info("Execute one new round of the inferring process")
