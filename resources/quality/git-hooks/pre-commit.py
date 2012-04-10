e2128d34452ca9bfbd46b9abeb41f3c16c8a6e1e
Georges Bossert <georges.bossert@supelec.fr>
2012-04-10 15:25:24 +0200

    Upgraded version of the pre-commit script.
    Apply PEP8 and search for conflicts patterns in the stagged area.

--- resources/quality/git-hooks/pre-commit.py
+++ resources/quality/git-hooks/pre-commit.py
@@ -27,29 +27,28 @@
 #+---------------------------------------------------------------------------+
 import os
 import re
-import subprocess
 import sys
-
-def stash():
-    # -u : include untracked files in the stashing process
-    # --keep-index : all changes already added to the index are left intact.
-    subprocess.call(['git', 'stash', 'save', '-u', '--keep-index', 'pre-commit stash'], stdout=subprocess.PIPE)
-
-def unstash():
-    # Unstash changes to the working tree that we had stashed
-    subprocess.call(['git', 'reset', '--hard'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    subprocess.call(['git', 'stash', 'pop', '-q'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+from git import *
     
 def getFiles():
-    files = []
-    regexToGetFile = re.compile('^(?:M|A)(\s+)(?P<name>.*)')
-    p = subprocess.Popen(['git', 'status', '--porcelain'], stdout=subprocess.PIPE)
-    out, err = p.communicate()
-    for line in out.splitlines():
-        match = regexToGetFile.match(line)
-        if match:
-            files.append(match.group('name'))
-    return files
+    currentPath = os.getcwd()
+    # First we initialize the repository object
+    repository = Repo(currentPath)    
+
+    listFile = []
+    repositoryIndex = repository.index
+    for d in repositoryIndex.diff('HEAD') :
+        # Added path
+        if d.deleted_file:
+            path = d.a_blob.path
+            if not path in listFile :
+                listFile.append(path)
+        elif not d.new_file :
+            path = d.a_blob.path
+            if not path in listFile :
+                listFile.append(path)
+    return listFile
+
 
 def checkPEP8(file) :
     localResult = []
@@ -59,15 +58,25 @@ def checkPEP8(file) :
         localResult.append(line)
     return localResult
 
+def searchForPattern(file, pattern, errorName) :
+    localResult = []
+    fileObject = open(file)
+    lineNumber = 0
+    for line in fileObject :
+        lineNumber += 1
+        if re.search(pattern, line):
+            localResult.append(str(errorName) + " found at line " + str(lineNumber))
+    return localResult
+
 def checkFile(file):
     results = dict()    
-        
-    # First we check against PEP8 rules
-    results['PEP8'] = checkPEP8(file)
+    
+    # Verify no '<<<' and or conflicts info are commited
+    results['Conflicts'] = searchForPattern(file, '<<<<<<', 'hints of untreated conflicts')
 
-    # ADD rules here
-
-
+    # Check against PEP8 rules for python files
+    if os.path.splitext(file)[-1] == ".py" :
+        results['PEP8'] = checkPEP8(file)
     
     return results
     
@@ -76,38 +85,33 @@ def verifyResults(results) :
     for file in results.keys() :
         resultFile = results[file]
         if len(resultFile) > 0 :
-            print file
+            print "[I] File %s :" % (file)
             for ruleName in resultFile.keys() :
                 ruleErrors = resultFile[ruleName]
                 for ruleError in ruleErrors :
-                    print "\t %s : %s" % (ruleName, ruleError)
+                    print "[I]\t %s : %s" % (ruleName, ruleError)
 
             result = 1
     return result
 
 
 def analyze():
-    # First we stash the current modifications
-    print "[I] Stashing the current modifications."
-    stash()
-    
-    # Retrieve all the files to analyze
-    print "[I] Retrieve all the files to analyze."
-    files = getFiles()
-    
-    print "[I] %d files will be analyzed." % (len(files))
-    globalResults = dict()
-    for file in files :
-        globalResults[file] = checkFile(file)
-    unstash()
+   # Retrieve all the files to analyze
+   print "[I] Retrieve all the files to analyze."
+   files = getFiles()
+   
+   print "[I] %d files will be analyzed." % (len(files))
+   globalResults = dict()
+   for file in files :
+       globalResults[file] = checkFile(file)
 
-    # Compute the final result (0=sucess, 1=cannot commit)
-    result = verifyResults(globalResults)
-    if result == 0 :
-        print "[I] No error found, commit allowed."
-    else :
-        print "[E] Errors founds, commit not allowed."
-    sys.exit(result)
+   # Compute the final result (0=sucess, 1=cannot commit)
+   result = verifyResults(globalResults)
+   if result == 0 :
+       print "[I] No error found, commit allowed."
+   else :
+       print "[E] Errors founds, commit not allowed."
+   sys.exit(result)
 
 
 if __name__ == '__main__' :
