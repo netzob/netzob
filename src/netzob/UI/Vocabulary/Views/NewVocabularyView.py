45e13560ce0b730af8aa4b9e3d5b76cd5d3e7a0b
Georges Bossert <georges.bossert@supelec.fr>
2012-07-27 10:17:02 +0200

    Reintroduce the verification of the current project before editing its vocabulary.

--- src/netzob/UI/Vocabulary/Views/NewVocabularyView.py
+++ src/netzob/UI/Vocabulary/Views/NewVocabularyView.py
@@ -389,15 +389,13 @@ class NewVocabularyView(object):
         return self.controller.netzob.getCurrentProject()
 
     def beginWithNetzob(self):
-        if len (self.getCurrentProject().getVocabulary().getMessages()) == 0:
-            if self.netzobBegin == None:
-                builder2 = Gtk.Builder()
-                builder2.add_from_file(os.path.join(
-                ResourcesConfiguration.getStaticResources(),
-                "ui",
-                "beginWithNetzob.glade"))
-                self.netzobBegin = builder2.get_object("netzobBegin")
-                self.messageTableBox.pack_start(self.netzobBegin, True , True, 0)
+        if self.getCurrentProject() != None and self.getCurrentProject().getVocabulary() is not None:
+            if len(self.getCurrentProject().getVocabulary().getMessages()) == 0:
+                if self.netzobBegin is None:
+                    builder2 = Gtk.Builder()
+                    builder2.add_from_file(os.path.join(ResourcesConfiguration.getStaticResources(), "ui", "beginWithNetzob.glade"))
+                    self.netzobBegin = builder2.get_object("netzobBegin")
+                    self.messageTableBox.pack_start(self.netzobBegin, True, True, 0)
 
         elif self.netzobBegin != None:
             self.netzobBegin.destroy()
