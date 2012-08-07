0bf3d75eba99a90e9f2d846c9085cc89a523bb43
Georges Bossert <georges.bossert@supelec.fr>
2012-08-07 16:18:14 +0200

    Remove "Begin Netzob" panel.
    Netzob's interface is not mature enough to support this functionnality.
    Should be reintroduced...

--- src/netzob/UI/Vocabulary/Views/NewVocabularyView.py
+++ src/netzob/UI/Vocabulary/Views/NewVocabularyView.py
@@ -221,7 +221,6 @@ class NewVocabularyView(object):
                                   str(sym.getID()))
         self.setSelectedSymbolFromSelectedMessageTable()
         self.symbolListTreeViewSelection.handler_unblock_by_func(self.controller.symbolListTreeViewSelection_changed_cb)
-        self.beginWithNetzob()
 
     def setSelectedSymbolFromSelectedMessageTable(self):
         if self.selectedMessageTable is None:
@@ -397,19 +396,6 @@ class NewVocabularyView(object):
 
     def getCurrentProject(self):
         return self.controller.netzob.getCurrentProject()
-
-    def beginWithNetzob(self):
-        if self.getCurrentProject() != None and self.getCurrentProject().getVocabulary() is not None:
-            if len(self.getCurrentProject().getVocabulary().getMessages()) == 0:
-                if self.netzobBegin is None:
-                    builder2 = Gtk.Builder()
-                    builder2.add_from_file(os.path.join(ResourcesConfiguration.getStaticResources(), "ui", "beginWithNetzob.glade"))
-                    self.netzobBegin = builder2.get_object("netzobBegin")
-                    self.messageTableBox.pack_start(self.netzobBegin, True, True, 0)
-
-        elif self.netzobBegin != None:
-            self.netzobBegin.destroy()
-            self.netzobBegin = None
 
     def getDisplayedSymbol(self):
         return self.selectedMessageTable.getDisplayedSymbol()
