a65cbafbb7a168872115e5492d53581a6981d5fe
Georges Bossert <georges.bossert@supelec.fr>
2012-08-16 19:09:24 +0200

    Fix and closes IssueId #293.

--- src/netzob/Inference/Vocabulary/Alignment/NeedlemanAndWunsch.py
+++ src/netzob/Inference/Vocabulary/Alignment/NeedlemanAndWunsch.py
@@ -304,7 +304,7 @@ class NeedlemanAndWunsch(object):
                             if fieldNext.getIndex() > field.getIndex():
                                 fieldNext.setIndex(fieldNext.getIndex() - 1)
                         # Concatenate the surrounding fields (because they should be static fields)
-                        if symbol.concatFields(field.getIndex() - 1) == 1:
+                        if symbol.concatFields(field.getIndex() - 1, field.getIndex()) == 1:
                             doLoop = True
                             break
 
