d20ea32bc263b4ccd35b796ffccba3ebc53c6e4c
Alexandre PIGNÉ <alex@freesenses.net>
2012-10-25 16:52:48 +0200

    Wireshark Exporter: Basic wireshark dissector plugin that uses symbols of a project

--- src/netzob_plugins/Exporters/WiresharkExporter/WiresharkExporter/WiresharkExporterController.py
+++ src/netzob_plugins/Exporters/WiresharkExporter/WiresharkExporter/WiresharkExporterController.py
@@ -26,10 +26,24 @@
 #+---------------------------------------------------------------------------+
 
 #+---------------------------------------------------------------------------+
+#| Standard library imports                                                  |
+#+---------------------------------------------------------------------------+
+from gettext import gettext as _
+import re
+import struct
+
+#+---------------------------------------------------------------------------+
 #| Local application imports                                                 |
 #+---------------------------------------------------------------------------+
+from netzob.Common.Models.L4NetworkMessage import L4NetworkMessage
+from netzob.Common.Models.L3NetworkMessage import L3NetworkMessage
+from netzob.Common.Models.L2NetworkMessage import L2NetworkMessage
 from netzob.Common.Plugins.Exporters.AbstractExporterController import AbstractExporterController
+from netzob.UI.NetzobWidgets import NetzobErrorMessage, NetzobWarningMessage
+from CodeBuffer import CodeBuffer
+from WiresharkExporterError import WiresharkExporterError
 from WiresharkExporterView import WiresharkExporterView
+from WiresharkFilter import WiresharkFilterFactory
 
 
 class WiresharkExporterController(AbstractExporterController):
@@ -38,10 +52,151 @@ class WiresharkExporterController(AbstractExporterController):
     """
     def __init__(self, netzob, plugin):
         view = WiresharkExporterView(plugin, self)
+        view.subscribe_signal('SymbolChanged', self._onSymbolChanged_cb)
         super(WiresharkExporterController, self).__init__(netzob, plugin, view)
 
+    def getMessageContext(self, msg):
+
+        def clean(s):
+            # Respect wireshark syntax.
+            # Allowed are lower characters, digits, '-', '_' and '.'
+            return re.sub("[^a-z\-_\.]", "_", str(s).lower())
+
+        sym = msg.getSymbol()
+        proto_name = clean(sym.getName())
+
+        proto_keyname = proto_name.upper()
+        proto_desc = "{} Protocol".format(proto_name.capitalize())
+        class_var = "proto_{}".format(msg.getID().replace('-', '_'))
+
+        if isinstance(msg, L4NetworkMessage):
+            filter_name = msg.getL4Protocol()
+        elif isinstance(msg, L3NetworkMessage):
+            filter_name = msg.getL3Protocol()
+        elif isinstance(msg, L2NetworkMessage):
+            filter_name = msg.getL2Protocol()
+        else:
+            raise ValueError("Cannot find a compatible protocol for {}.".format(msg))
+
+        del sym
+
+        return locals()
+
+    def generateSymbolDissector(self, sym):
+        msg_ref = sym.getMessages()[0]
+        ctx = self.getMessageContext(msg_ref)
+        buf = CodeBuffer()
+        buf.write("--\n-- Symbol {proto_keyname}\n--\n".format(**ctx))
+        buf.write("""{class_var} = Proto("{proto_name}", "{proto_name} Protocol")
+function {class_var}.dissector(buffer, pinfo, tree)
+  pinfo.cols.protocol = "{proto_keyname}"
+  local subtree = tree:add({class_var}, buffer(), "{proto_desc}")
+""".format(**ctx))
+        i = 0
+
+        # XXX: do not use msg_ref here, not valid for all msgs
+        fields = sym.getExtendedFields()
+        splittedData = msg_ref.getSplittedData(fields, msg_ref.data)
+        for field, ivalue in zip(fields, splittedData):
+            value = ivalue.decode('hex')
+            fmt = field.getFormat()
+
+            # raise an error when a non-trailing dynamic regex is encountered
+            if field.isRegexOnlyDynamic() and field.getIndex() < len(fields) - 1:
+                raise WiresharkExporterError(_("Cannot handle dynamic-size fields"))
+
+            with buf:
+                buf.write('if buffer({start}):len() > 0 then\n'.format(start=i))
+                j = len(value)
+                with buf:
+                    if field.isRegexOnlyDynamic():
+                        buf.write('subtree:add(buffer({start}), "{prefix}: " .. buffer({start})'
+                                  .format(start=i, prefix=field.getName()))
+                    else:
+                        buf.write('subtree:add(buffer({start},{length}), "{prefix}: " .. buffer({start},{length})'
+                                  .format(start=i, length=j, prefix=field.getName()))
+                    buf_type = _getLuaTvbType(field)
+                    if buf_type is not None:
+                        buf.write(':{}()'.format(buf_type))
+                    buf.write(')\n')
+            buf.write('end\n')
+            i += j
+        buf.write("end\n")  # end of dissector function
+
+        # Register dissector function to specific filter criterion
+        filter_ = WiresharkFilterFactory.getFilter(sym)
+        luatype = _getLuaTableType(filter_.pytype)
+        for expr in filter_.getExpressions():
+            buf.write("""if not pcall(DissectorTable.get, "{0}") then
+  DissectorTable.new("{0}", "Netzob-generated table", {type})
+end
+DissectorTable.get("{0}"):add({1}, {class_var})
+""".format(*expr, type=luatype, **ctx))
+
+        return buf.getvalue()
+
     def run(self):
-        pass
+        data = []
+        proj = self.netzob.getCurrentProject()
+
+        if not proj:
+            NetzobErrorMessage(_("No project selected."))
+            return
+
+        syms = proj.getVocabulary().getSymbols()
+        dial = self.view.buildDialog()
+        self.view.updateSymbols(syms)
+        dial.show_all()
 
     def __call__(self):
         self.run()
+
+    ##########
+    # Events #
+    ##########
+    def _onSymbolChanged_cb(self, tv):
+        sel = tv.get_selection()
+
+        if not sel:
+            return  # on closing, event occurs
+
+        tm, it = sel.get_selected()
+        sym_id = tm.get_value(it, 0)
+        voca = self.netzob.getCurrentProject().getVocabulary()
+
+        sym = voca.getSymbol(sym_id)
+        if sym is None:
+            self.view.clearText()
+            for sym in voca.getSymbols():
+                try:
+                    self.view.appendText(self.generateSymbolDissector(sym))
+                except WiresharkExporterError, wee:
+                    self.view.appendText("-- {}\n".format(wee))
+                    NetzobWarningMessage("[{}] {}".format(sym.getName(), wee))
+        else:
+            try:
+                self.view.setText(self.generateSymbolDissector(sym))
+            except WiresharkExporterError, wee:
+                NetzobErrorMessage("[{}] {}".format(sym.getName(), wee))
+
+
+def _getLuaTableType(pytype):
+    if issubclass(pytype, (int, long)):
+        return 'ftypes.UINT32'
+    if issubclass(pytype, basestring):
+        return 'ftypes.STRING'
+    raise ValueError("Cannot match LUA type for {!s}".format(pytype))
+
+
+def _getLuaTvbType(field):
+    fmt = field.getFormat()
+    typ = None
+    if fmt in ['string', 'ipv4']:
+        typ = fmt
+    if 'decimal' == fmt:
+        typ = 'uint'
+# XXX: bug in lua
+#    if 'binary' == fmt: typ = 'bytes'
+    if 'little-endian' == field.getEndianess() and typ:
+        typ = 'le_{}'.format(typ)
+    return typ
--- src/netzob_plugins/Exporters/WiresharkExporter/WiresharkExporter/WiresharkExporterView.py
+++ src/netzob_plugins/Exporters/WiresharkExporter/WiresharkExporter/WiresharkExporterView.py
@@ -26,15 +26,101 @@
 #+---------------------------------------------------------------------------+
 
 #+---------------------------------------------------------------------------+
+#| Standard library imports                                                  |
+#+---------------------------------------------------------------------------+
+from gettext import gettext as _
+
+#+---------------------------------------------------------------------------+
+#| Related third party imports                                               |
+#+---------------------------------------------------------------------------+
+from gi.repository import Gtk, Pango
+
+#+---------------------------------------------------------------------------+
 #| Local application imports                                                 |
 #+---------------------------------------------------------------------------+
+from netzob.Common.Pattern.Observer import Observable
 from netzob.Common.Plugins.Exporters.AbstractExporterView import AbstractExporterView
 
 
-class WiresharkExporterView(AbstractExporterView):
+class WiresharkExporterView(AbstractExporterView, Observable):
     """
     GUI for exporting results in Wireshark LUA script.
     """
 
     def __init__(self, plugin, controller):
         super(WiresharkExporterView, self).__init__(plugin, controller)
+        self.register_signals('SymbolChanged')
+
+    def getDialog(self):
+        return self.dialog
+
+    def buildDialog(self):
+        self.buffer = self.sym_store = None
+        self.panel = self.buildPanel()
+        self.panel.set_size_request(800, 600)
+        self.dialog = Gtk.Dialog(_("Export project as Wireshark dissector"),
+                                 flags=0, buttons=None)
+        self.dialog.vbox.pack_start(self.panel, True, True, 0)
+        return self.dialog
+
+    def buildPanel(self):
+        h_pan = Gtk.HPaned()
+        h_pan.add1(self.buildSymbolTreeview())
+        h_pan.add2(self.buildScriptView())
+        v_box = Gtk.VBox()
+        v_box.pack_start(self.buildToolbar(), False, False, 0)
+        v_box.pack_end(h_pan, True, True, 0)
+        v_box.set_focus_child(h_pan)
+        return v_box
+
+    def buildSymbolTreeview(self):
+        self.sym_store = Gtk.TreeStore(str, str, str)
+        sym_tv = Gtk.TreeView(self.sym_store)
+        sym_tv.connect("cursor-changed", lambda tv: self._send_signal('SymbolChanged', tv))
+
+        rdr = Gtk.CellRendererText()
+        col_syms = Gtk.TreeViewColumn(_("Symbols"), rdr, text=1)
+        sym_tv.append_column(col_syms)
+
+        sym_tv_scroll = Gtk.ScrolledWindow()
+        sym_tv_scroll.set_size_request(150, -1)
+        sym_tv_scroll.add(sym_tv)
+        return sym_tv_scroll
+
+    def buildScriptView(self):
+        sw = Gtk.ScrolledWindow()
+        textarea = Gtk.TextView()
+        textarea.set_editable(False)
+        textarea.modify_font(Pango.FontDescription('monospace'))
+        self.buffer = textarea.get_buffer()
+        sw.add(textarea)
+        return sw
+
+    def updateSymbols(self, syms):
+        self.sym_store.clear()
+        self.sym_store.append(None, ("-1", "{} [{}]".format(_("Entire project"), len(syms)), "0"))
+        for sym in syms:
+            self.sym_store.append(None, (str(sym.getID()), "{} [{}]".format(sym.getName(), len(sym.getMessages())), str(sym.field.getScore())))
+
+    def getText(self):
+        return self.buffer.get_text(self.buffer.get_start_iter(), self.buffer.get_end_iter(), True)
+
+    def clearText(self):
+        self.buffer.set_text("")
+
+    def setText(self, txt):
+        self.clearText()
+        self.appendText(txt)
+
+    def appendText(self, txt):
+        self.buffer.insert(self.buffer.get_end_iter(), txt)
+
+    ##########
+    # Events #
+    ##########
+    def __onSymbolChanged_cb(self, tv, *args):
+        for cb_f, cb_args in self.__cursor_changed_obs:
+            cb_f(tv, *cb_args)
+
+    def onSymbolChanged(self, func, *args):
+        self.__cursor_changed_obs.add((func, args))
