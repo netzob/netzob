cb3bc87e908630a85d133a654388a317c339c5e9
Olivier Tétard <olivier.tetard@miskin.fr>
2012-11-06 22:01:27 +0100

    Unmark some debug as translatable

--- src/netzob/Common/MMSTD/Dictionary/VariableProcessingToken/VariableWritingToken.py
+++ src/netzob/Common/MMSTD/Dictionary/VariableProcessingToken/VariableWritingToken.py
@@ -65,7 +65,7 @@ class VariableWritingToken(AbstractVariableProcessingToken):
         """toString:
                 Used for debug purpose.
         """
-        return _("WritingToken: isOk: {0}, value: {1}").format(str(self.isOk()), TypeConvertor.bin2strhex(self.value))
+        return "WritingToken: isOk: {0}, value: {1}".format(str(self.isOk()), TypeConvertor.bin2strhex(self.value))
 
     def updateValue(self):
         """updateValue:
