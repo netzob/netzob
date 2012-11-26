967c629bae31af2544f3429ff7f71f9172768dcb
Olivier Tétard <olivier.tetard@miskin.fr>
2012-11-26 12:46:08 +0100

    PeachExporter: Update setup.py to install glade files in the correct path

--- src/netzob_plugins/Exporters/PeachExporter/setup.py
+++ src/netzob_plugins/Exporters/PeachExporter/setup.py
@@ -30,6 +30,15 @@
 #| Global Imports
 #+----------------------------------------------------------------------------
 from setuptools import setup
+import sys
+
+package = 'PeachExporter'
+resourcesPath = "../../../../resources/"
+
+sys.path.append(resourcesPath)
+from sdist.utils import find_data_files, opj
+
+pluginsStaticResourcesPath = opj(resourcesPath, "static/netzob_plugins/", package)
 
 dependencies = [
     'Netzob > 0.4'
@@ -43,8 +52,12 @@ setup(
     version="1.0.0",
     author="Benjamin Dufour",
     author_email="contact@netzob.org",
-    packages=['PeachExporter'],
+    packages=[package],
     install_requires=dependencies,
+    data_files=find_data_files(opj("share", "netzob", "plugins"),
+                               pluginsStaticResourcesPath,
+                               '*.glade',
+                               recursive=True),
     entry_points="""
     [netzob.plugins]
     PeachExporter=PeachExporter.PeachExporterPlugin:PeachExporterPlugin
