2fbde2d814fe69962567124ded9eee664ed1bbbd
Olivier Tétard <olivier.tetard@miskin.fr>
2012-12-13 17:59:02 +0100

    Trace Manager: Add a new TraceManagerController and TraceManagerView.
    These two classes inherits from NetzobPerspective{View,Controller}
    that simply display the 'Trace Manager' perspective.
    
    Change-Id: I6280c410ac71804b99452df046e6db2f15fcd887

---
+++ src/netzob/UI/TraceManager/Controllers/TraceManagerController.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+#+---------------------------------------------------------------------------+
+#|          01001110 01100101 01110100 01111010 01101111 01100010            |
+#|                                                                           |
+#|               Netzob : Inferring communication protocols                  |
+#+---------------------------------------------------------------------------+
+#| Copyright (C) 2011 Georges Bossert and Frédéric Guihéry                   |
+#| This program is free software: you can redistribute it and/or modify      |
+#| it under the terms of the GNU General Public License as published by      |
+#| the Free Software Foundation, either version 3 of the License, or         |
+#| (at your option) any later version.                                       |
+#|                                                                           |
+#| This program is distributed in the hope that it will be useful,           |
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
+#| GNU General Public License for more details.                              |
+#|                                                                           |
+#| You should have received a copy of the GNU General Public License         |
+#| along with this program. If not, see <http://www.gnu.org/licenses/>.      |
+#+---------------------------------------------------------------------------+
+#| @url      : http://www.netzob.org                                         |
+#| @contact  : contact@netzob.org                                            |
+#| @sponsors : Amossys, http://www.amossys.fr                                |
+#|             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
+#+---------------------------------------------------------------------------+
+
+#+---------------------------------------------------------------------------+
+#| Standard library imports
+#+---------------------------------------------------------------------------+
+from gettext import gettext as _
+import logging
+from gi.repository import Gtk
+
+#+---------------------------------------------------------------------------+
+#| Related third party imports
+#+---------------------------------------------------------------------------+
+
+#+---------------------------------------------------------------------------+
+#| Local application imports
+#+---------------------------------------------------------------------------+
+from netzob.UI.TraceManager.Views.TraceManagerView import TraceManagerView
+from netzob.UI.NetzobAbstractPerspectiveController import NetzobAbstractPerspectiveController
+
+
+class TraceManagerController(NetzobAbstractPerspectiveController):
+    """Trace Manager Controller. This view allows to manage traces
+    that were imported in Netzob. This panel is accessible through the
+    main view, using the switch panel dropdown."""
+
+    def __init__(self, mainController):
+        super(TraceManagerController, self).__init__(mainController, TraceManagerView)
