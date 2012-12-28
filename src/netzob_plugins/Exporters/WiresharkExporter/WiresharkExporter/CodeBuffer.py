c9143dd471ad64109a92386bbdf986f6a2694530
Alexandre PIGNÉ <alex@freesenses.net>
2012-12-28 11:41:27 +0100

    Wireshark Exporter: introduce CodeBuffer that implements context management.
    To provide indentation, please use the with statement like this:
    > buf = CodeBuffer()
    > buf.write("1")
    > with buf:
    >   buf.write("2")
    >   with buf:
    >     buf.write("3")
    >   buf.write("4")
    
    To produce:
    > 1
    >   2
    >     3
    >   4

---
+++ src/netzob_plugins/Exporters/WiresharkExporter/WiresharkExporter/CodeBuffer.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+#+---------------------------------------------------------------------------+
+#|          01001110 01100101 01110100 01111010 01101111 01100010            |
+#|                                                                           |
+#|               Netzob : Inferring communication protocols                  |
+#+---------------------------------------------------------------------------+
+#| Copyright (C) 2011 AMOSSYS                                                |
+#| This program is free software: you can redistribute it and/or modify      |
+#| it under the terms of the GNU General Public License as published by      |
+#| the Free Software Foundation, either version 3 of the License, or         |
+#| (at your option) any later version.                                       |
+#|                                                                           |
+#| This program is distributed in the hope that it will be useful,           |
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
+#| GNU General Public License for more details.                              |
+#|                                                                           |
+#| You should have received a copy of the GNU General Public License         |
+#| along with this program. If not, see <http://www.gnu.org/licenses/>.      |
+#+---------------------------------------------------------------------------+
+#| @url      : http://www.netzob.org                                         |
+#| @contact  : contact@netzob.org                                            |
+#| @sponsors : Amossys, http://www.amossys.fr                                |
+#|             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
+#+---------------------------------------------------------------------------+
+
+#+---------------------------------------------------------------------------+
+#| Standard library imports                                                  |
+#+---------------------------------------------------------------------------+
+from StringIO import StringIO
+
+
+class CodeBuffer(StringIO):
+    """
+    StringIO class with indentation capabilities
+    """
+    INDENT_SIZE = 2
+
+    def __init__(self, *args, **kwargs):
+        StringIO.__init__(self, *args, **kwargs)
+        self._indent = 0
+
+    def write(self, s):
+        indent_s = ' ' * (self._indent * self.INDENT_SIZE) + s
+        StringIO.write(self, indent_s)
+
+    def indent(self):
+        self._indent += 1
+    __enter__ = indent
+
+    def dedent(self):
+        self._indent -= 1
+    __exit__ = lambda self, *args: self.dedent()
