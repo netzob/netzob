975a322c1a361b3f6fc056d95e21a8ff5fe0bd4c
Georges Bossert <georges.bossert@supelec.fr>
2013-03-12 00:04:24 +0100

    Creates a parseMessages to extract messages provided by Python.
    Change-Id: I01370216a875d8b30fa9081f82d6dabb723630f6

--- lib/argsFactories/factory.c
+++ lib/argsFactories/factory.c
@@ -76,8 +76,120 @@ int parseArgs(PyObject* factobj, ...){
 
 }
 
+void parseLibscoreComputation(PyObject* factobj, va_list args){
+  long i;
+  PyObject* pysize = NULL;
+  long* nbmess = va_arg(args,long*);
+  t_message** messages = va_arg(args,t_message**);
+  unsigned int debugMode = FALSE;
+
+  /**
+     list : which is a list of messages
+  */
+  PyObject* list = PyObject_GetAttr(factobj,PyString_FromString("args"));
+
+  /**
+     Find the number of elements in the list.
+     This number of elements = number of messages (nbmess)
+  */
+  pysize = PyLong_FromSsize_t(PyList_Size(list));
+  *nbmess = PyLong_AsLong(pysize);
+  Py_XDECREF(pysize);
+
+  /**
+     Reserves an array of [nbmess] t_messages
+  */
+  *messages = (t_message*) malloc((*nbmess)*sizeof(t_message));
+
+  /**
+     Parse each message and store them in the newly allocated array
+  */
+  for(i=0;i<*nbmess;i++){
+    PyObject* item;
+    item = PyList_GetItem(list,(Py_ssize_t)i);
+    parseMessage(item, &((*messages)[i]));
+  }
+
+  // [DEBUG] Display the content of the deserialized messages
+  if (debugMode == TRUE) {
+    unsigned int iMessage;
+    for(iMessage=0;iMessage<*nbmess;iMessage++) {
+      printf("Message : %d\n", iMessage);
+      printf("Data : ");
+      t_message message = (*messages)[iMessage];
+
+      for (i=0; i< message.len; i++) {
+	printf("%02x", (unsigned char) message.alignment[i]);
+      }
+      printf("\n");
+      printf("Tags : ");
+      for (i=0; i< message.len; i++) {
+	if (message.semanticTags != NULL && message.semanticTags[i] != NULL && message.semanticTags[i]->name != NULL && strcmp(message.semanticTags[i]->name, "None")!=0) {
+	  printf("!!");
+	} else {
+	  printf("..");
+	}
+      }
+      printf("\n");
+    }
+    // [DEBUG]
+  }
+}
+
+void parseMessage(PyObject * item, t_message * message) {
+  char * tmp_alignment;
+  unsigned int j;
+  /**
+     message.alignment contains the message.getReducedStringData() in python raw format. Its the content of the message. If its during orphan reduction, this content is reduced to the considered section (sliding window).
+  */
+  tmp_alignment = getstringattr(item,"alignment");
+  message->alignment = (unsigned char*) tmp_alignment;
+
+  /**
+     message->mask will be allocated (no value in it yet) to contain at least ... ?
+  */
+  message->mask = calloc(strlen(tmp_alignment)+1,sizeof(unsigned char*));
+
+  /**
+     message->len contains the size of tmp_alignment
+  **/
+  message->len = (unsigned int) getUnsignedLongAttribute(item,"length");
+
+  /**
+     message->semanticTags contains the list of tags attached to each half-byte of the alignment
+  */
+  message->semanticTags = calloc(message->len, sizeof(t_semanticTag *));
+
+  // retrieve the list of tags
+  PyObject* listOfSemanticTags = PyObject_GetAttr(item,tostring("semanticTags"));
+
+  // verify its a list
+  if (PyList_CheckExact(listOfSemanticTags) && message->len == (unsigned int)PyList_Size(listOfSemanticTags)) {
+    // every half-byte should be tagged (with no-tag or with a real tag)
+    // parse all the tags
+    for (j=0; j<message->len; j++) {
+      PyObject * listItem = PyList_GetItem(listOfSemanticTags,(Py_ssize_t)j);
+      char * tag = PyString_AsString(listItem);
+
+      message->semanticTags[j] = malloc(sizeof(t_semanticTag));
+      message->semanticTags[j]->name = tag;
+    }
+  } else {
+    printf("[C-Extension] Error while parsing semantic tags.\n");
+  }
+  /**
+     message->uid contains the UID of the symbol which contains
+     the message.
+
+     Warning: I though it was message's UID, but its not !!
+  */
+  message->uid = getstringattr(item,"uid");
+
+}
+
+
 /**
-   parseLibscoreComputation:
+   parseLibNeedlman:
 
    This function parses the arguments wrapper following a specific format.
    The definition of this format can be found in the Python function:
