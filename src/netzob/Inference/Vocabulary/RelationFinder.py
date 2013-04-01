f61ea6264ce694fe38e84be72e0971d782f8dc8a
Frédéric Guihéry <frederic.guihery@amossys.fr>
2013-04-01 18:17:35 +0200

    Add new relation finding functions

--- src/netzob/Inference/Vocabulary/RelationFinder.py
+++ src/netzob/Inference/Vocabulary/RelationFinder.py
@@ -29,6 +29,7 @@
 #| Global Imports
 #+----------------------------------------------
 import logging
+import uuid
 
 #+----------------------------------------------
 #| Local Imports
@@ -68,3 +69,81 @@ class RelationFinder(object):
                     print "Relations(%s) with F%d:" % (algo, ref_idx)
                     for rel_id, rel_conf in enumerate(rels):
                         print "  %d. F[%d][%d:%d]" % ((rel_id,) + rel_conf)
+
+    #+----------------------------------------------
+    #| executeOnCells:
+    #| @param cellsTable : a table of cells
+    #+----------------------------------------------
+    def executeOnCells(self, cellsTable):
+        if cellsTable:
+            # Invert array dimensions liks this:
+            # < [[m0f0, m1f0, ...], [m0f1, m1f1, ...]]
+            # > [(m0f0, m0f1, ...), (m1f0, m1f1, ...)]
+            for algo, refs in _libRelation.find(zip(*cellsTable)).items():
+                for ref_idx, ref_off, ref_size, rels in refs:
+                    print "Relations(%s) with F%d:" % (algo, ref_idx)
+                    for rel_id, rel_conf in enumerate(rels):
+                        print "  %d. F[%d][%d:%d]" % ((rel_id,) + rel_conf)
+
+    #+----------------------------------------------
+    #| executeOnCellsWithAttributes:
+    #+----------------------------------------------
+    def executeOnCellsWithAttributes(self, x_field, x_attr, y_field, y_attr):
+        # Convert cells according to their interesting attribute (data, size or offset)
+        if x_attr == "s" and y_attr == "s":  # Two size field are uncertain...
+            return
+        x_values = x_field.getCells()
+        y_values = y_field.getCells()
+
+        # Try to find a relation that matches each cell
+        results = []
+        relation_fcts = {}
+        relation_fcts["size"] = self.sizeRelation
+        relation_fcts["equality"] = self.equalRelation
+
+        for (relation_name, relation_fct) in relation_fcts.items():
+            isRelation = True
+            for i in range(len(x_values)):
+                if not relation_fct(x_values[i], x_attr, y_values[i], y_attr):
+                    isRelation = False
+                    break
+            if isRelation:
+                logging.info("Relation found between '" + x_attr + ":" + x_field.getName() + "' and '" + y_attr + ":" + y_field.getName() + "'")
+                logging.info("  Relation: " + relation_name)
+                id_relation = str(uuid.uuid4())
+                results.append({'id': id_relation,
+                                "relation_type": relation_name,
+                                'x_field': x_field,
+                                'x_attribute': x_attr,
+                                'y_field': y_field,
+                                'y_attribute': y_attr})
+        return results
+
+    def equalRelation(self, x, x_attr, y, y_attr):
+        if x == y:
+            return True
+        else:
+            return False
+
+    def sizeRelation(self, x, x_attr, y, y_attr):
+        if x_attr == "s":
+            if len(x) > 0:
+                x = len(x) / 2
+        else:
+            if len(x) > 0:
+                x = int(x, 16)
+            else:
+                x = 0
+        if y_attr == "s":
+            if len(y) > 0:
+                y = len(y) / 2
+        else:
+            if len(y) > 0:
+                y = int(y, 16)
+            else:
+                y = 0
+
+        if x == y:
+            return True
+        else:
+            return False
