ad128f02556d0179c9402a0dc661efe2d19bb1a5
Georges Bossert <georges.bossert@supelec.fr>
2013-06-08 12:54:45 +0200

    Initate some Tutorials tests.

---
+++ test/src/test_netzob/test_Tutorials/test_ICMP.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+#+---------------------------------------------------------------------------+
+#|          01001110 01100101 01110100 01111010 01101111 01100010            |
+#|                                                                           |
+#|               Netzob : Inferring communication protocols                  |
+#+---------------------------------------------------------------------------+
+#| Copyright (C) 2011 Georges Bossert and Frédéric Guihéry                   |
+#| This program is free software: you can redistribute it and/or modify      |
+#| it under the terms of the GNU General Public License as published by      |
+#| the Free Software Foundation, either version 3 of the License, or         |
+#| (at your option) any later version.                                       |
+#|                                                                           |
+#| This program is distributed in the hope that it will be useful,           |
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
+#| GNU General Public License for more details.                              |
+#|                                                                           |
+#| You should have received a copy of the GNU General Public License         |
+#| along with this program. If not, see <http://www.gnu.org/licenses/>.      |
+#+---------------------------------------------------------------------------+
+#| @url      : http://www.netzob.org                                         |
+#| @contact  : contact@netzob.org                                            |
+#| @sponsors : Amossys, http://www.amossys.fr                                |
+#|             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
+#+---------------------------------------------------------------------------+
+
+#+---------------------------------------------------------------------------+
+#| Standard library imports
+#+---------------------------------------------------------------------------+
+import logging
+
+#+---------------------------------------------------------------------------+
+#| Local Imports
+#+---------------------------------------------------------------------------+
+from common.NetzobTestCase import NetzobTestCase
+from netzob import *
+
+
+class test_ICMP(NetzobTestCase):
+
+    def test_generateICMP(self):
+        """test_generateICMP:
+        """
+        logging.debug("Test : generate ICMP.")
+
+        icmp = Protocol("ICMP")
+        pingHeader = [Field(8), Field(0), Field(0)]
+        pingPayload = Field(ASCII(size=50))
+        pingHeader[2] = Field(CRC16(pingHeader[0:2] + [pingPayload]))
+        ping = Symbol(pingHeader + [pingPayload])
+        print ping.generate()
