93c392826b2bfac7163f9fd63642278f27e53f73
Georges Bossert <georges.bossert@supelec.fr>
2013-06-10 18:29:46 +0200

    New version of Messages with a simple AbstractMessage and RawMessage (WIP).

---
+++ src/netzob/Common/Models/Vocabulary/Messages/RawMessage.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+#+---------------------------------------------------------------------------+
+#|          01001110 01100101 01110100 01111010 01101111 01100010            |
+#|                                                                           |
+#|               Netzob : Inferring communication protocols                  |
+#+---------------------------------------------------------------------------+
+#| Copyright (C) 2011 Georges Bossert and Frédéric Guihéry                   |
+#| This program is free software: you can redistribute it and/or modify      |
+#| it under the terms of the GNU General Public License as published by      |
+#| the Free Software Foundation, either version 3 of the License, or         |
+#| (at your option) any later version.                                       |
+#|                                                                           |
+#| This program is distributed in the hope that it will be useful,           |
+#| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
+#| GNU General Public License for more details.                              |
+#|                                                                           |
+#| You should have received a copy of the GNU General Public License         |
+#| along with this program. If not, see <http://www.gnu.org/licenses/>.      |
+#+---------------------------------------------------------------------------+
+#| @url      : http://www.netzob.org                                         |
+#| @contact  : contact@netzob.org                                            |
+#| @sponsors : Amossys, http://www.amossys.fr                                |
+#|             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
+#+---------------------------------------------------------------------------+
+
+#+---------------------------------------------------------------------------+
+#| Standard library imports
+#+---------------------------------------------------------------------------+
+
+#+---------------------------------------------------------------------------+
+#| Related third party imports
+#+---------------------------------------------------------------------------+
+
+#+---------------------------------------------------------------------------+
+#| Local application imports
+#+---------------------------------------------------------------------------+
+from netzob.Common.Models.Vocabulary.Messages.AbstractMessage import AbstractMessage
+
+
+class RawMessage(AbstractMessage):
+    """Represents a raw Message which is a single message with some content and very few meta-data.
+
+    >>> msg = RawMessage("That's a simple message")
+    >>> msg = RawMessage(0x123456789)
+
+    """
+
+    def __init__(self, data=None):
+        """
+        :parameter data: the content of the message
+        :type data: a :class:`object`
+        """
+        super(RawMessage, self).__init__(data)
