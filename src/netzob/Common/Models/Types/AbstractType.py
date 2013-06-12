43c12bcbfcc5d3e009542d2411ac972e3965c9fa
Georges Bossert <georges.bossert@supelec.fr>
2013-06-12 19:09:38 +0200

    AbstractType stores the initial type value and size

--- src/netzob/Common/Models/Types/AbstractType.py
+++ src/netzob/Common/Models/Types/AbstractType.py
@@ -34,6 +34,7 @@
 #+---------------------------------------------------------------------------+
 #| Standard library imports                                                  |
 #+---------------------------------------------------------------------------+
+import abc
 
 #+---------------------------------------------------------------------------+
 #| Related third party imports                                               |
@@ -47,10 +48,89 @@ from netzob.Common.Utils.Decorators import typeCheck
 
 class AbstractType(object):
 
-    def __init__(self, typeName, value=None, size=None):
-        self.__typeName = typeName
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, typeName, value, size=(None, None)):
+        self.typeName = typeName
+        self.value = value
+        self.size = size
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
         self.__value = value
-        self.__size = size
+
+    @property
+    def size(self):
+        """The size of the expected Type defined
+         by a tuple (min, max).
+         The value 'None' can be set for min and/or max to represent no limitations.
+
+         For instance, to create an ASCII field of at least 10 chars:
+
+         >>> from netzob import *
+         >>> f = Field(ASCII(size=(10,None)))
+
+         while to create a Raw field which content has no specific limit:
+
+         >>> from netzob import *
+         >>> f = Field(Raw())
+
+         :type: tuple (int, int)
+         :raises: :class:`TypeError` or :class:`ValueError` if parameters are not valid.
+
+         """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+
+        if size is None:
+            self.__size = (None, None)
+        elif isinstance(size, tuple):
+            minSize, maxSize = size
+
+            if minSize is not None and not isinstance(minSize, int):
+                raise TypeError("Size must be defined with a tuple of int")
+            if maxSize is not None and not isinstance(maxSize, int):
+                raise TypeError("Size must be defined with a tuple of int")
+
+            if minSize is None:
+                minSize = 0
+
+            if minSize < 0:
+                raise ValueError("Minimum size must be greater than 0")
+            if maxSize is not None and maxSize < minSize:
+                raise ValueError("Maximum must be greater or equals to the minimum")
+
+            self.__size = (minSize, maxSize)
+        else:
+            raise TypeError("Size must be defined by a tuple or with None")
+
+    @abc.abstractmethod
+    def buildDataRepresentation(self):
+        """It creates a :class:`netzob.Common.Models.Vocabulary.Domain.Variables.Leafs.Data.Data` following the specified type.
+
+        for instance, user can specify a domain with its type which is much more simple than creating a Data with the type
+
+        >>> from netzob import *
+        >>> ascii = ASCII("hello netzob !")
+        >>> print ascii.typeName
+        ASCII
+        >>> data = ascii.buildDataRepresentation()
+        >>> print data.currentValue
+        hello netzob !
+        >>> print data.dataType.__name__
+        ASCII
+
+        :return: a Data of the current type
+        :rtype: :class:`netzob.Common.Models.Vocabulary.Domain.Variables.Leads.Data.Data`
+
+        """
+        return
 
     @property
     def typeName(self):
@@ -65,12 +145,6 @@ class AbstractType(object):
     @typeName.setter
     @typeCheck(str)
     def typeName(self, typeName):
+        if typeName is None:
+            raise TypeError("typeName cannot be None")
         self.__typeName = typeName
-
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, value):
-        self.__value = value
