e98b9bd284e742cd72bf787b8cc4f6dbabd04063
Georges Bossert <georges.bossert@supelec.fr>
2013-09-19 09:53:51 +0200

    Expose visualization functions to the user API

--- src/netzob/Common/Models/Vocabulary/Functions/all.py
+++ src/netzob/Common/Models/Vocabulary/Functions/all.py
@@ -29,3 +29,4 @@
 # List subpackages to import with the current one
 # see docs.python.org/2/tutorial/modules.html
 from netzob.Common.Models.Vocabulary.Functions.EncodingFunctions.all import *
+from netzob.Common.Models.Vocabulary.Functions.VisualizationFunctions.all import *
