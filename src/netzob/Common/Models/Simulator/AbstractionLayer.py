6331f8c7caab25e5c0358f36b8fec330ac168188
Frédéric Guihéry <frederic.guihery@amossys.fr>
2014-01-02 21:52:37 +0100

    Make AbstractionLayer class functional

--- src/netzob/Common/Models/Simulator/AbstractionLayer.py
+++ src/netzob/Common/Models/Simulator/AbstractionLayer.py
@@ -42,19 +42,41 @@
 #+---------------------------------------------------------------------------+
 #| Local application imports                                                 |
 #+---------------------------------------------------------------------------+
-from netzob.Common.Utils.Decorators import typeCheck
+from netzob.Common.Utils.Decorators import typeCheck, NetzobLogger
 from netzob.Common.Models.Vocabulary.Symbol import Symbol
 from netzob.Common.Models.Vocabulary.EmptySymbol import EmptySymbol
+from netzob.Common.Models.Vocabulary.AbstractField import AbstractField
 
 
+@NetzobLogger
 class AbstractionLayer(object):
-    """An abstraction layer specializes a symbol into a message before emitting it and on the
-    other way, abstracts a received message into a symbol.
+    """An abstraction layer specializes a symbol into a message before
+    emitting it and on the other way, abstracts a received message
+    into a symbol.
+
+    >>> from netzob.all import *
+    >>> symbol = Symbol([Field("Hello Zoby !")], name = "Symbol_Hello")
+
+    >>> channelIn = UDPServer(localIP="127.0.0.1", localPort=8888)
+    >>> abstractionLayerIn = AbstractionLayer(channelIn, [symbol])
+    >>> abstractionLayerIn.openChannel()
+
+    >>> channelOut = UDPClient(remoteIP="127.0.0.1", remotePort=8888)
+    >>> abstractionLayerOut = AbstractionLayer(channelOut, [symbol])
+    >>> abstractionLayerOut.openChannel()
+
+    >>> abstractionLayerOut.writeSymbol(symbol)
+    >>> (receivedSymbol, receivedMessage) = abstractionLayerIn.readSymbol()
+    >>> print receivedSymbol.name
+    Symbol_Hello
+    >>> print receivedMessage
+    Hello Zoby !
 
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, channel, symbols):
+        self.channel = channel
+        self.symbols = symbols
 
     @typeCheck(Symbol)
     def writeSymbol(self, symbol):
@@ -68,7 +90,13 @@ class AbstractionLayer(object):
         if symbol is None:
             raise TypeError("The symbol to write on the channel cannot be None")
 
-        pass
+        self._logger.info("Going to specialize symbol: '{0}' (id={1}).".format(symbol.name, symbol.id))
+        data = symbol.specialize()
+        self._logger.info("Data generated from symbol '{0}': {1}.".format(symbol.name, repr(data)))
+
+        self._logger.info("Going to write to communication channel...")
+        self.channel.write(data)
+        self._logger.info("Writing to commnunication channel donne..")
 
     @typeCheck(int)
     def readSymbol(self, timeout=EmptySymbol.defaultReceptionTimeout()):
@@ -82,4 +110,24 @@ class AbstractionLayer(object):
         :type timeout: :class:`int`
         :raise TypeError if the parameter is not valid and Exception if an error occurs.
         """
-        pass
+        self._logger.info("Going to read from communication channel...")
+        data = self.channel.read()
+        self._logger.info("Received data: '{0}'".format(repr(data)))
+
+        symbol = AbstractField.abstract(data, self.symbols)
+        if symbol is not None:
+            self._logger.info("Received symbol on communication channel: '{0}'".format(symbol.name))
+        else:
+            self._logger.info("Received symbol on communication channel: '{0}'".format(symbol))
+
+        return (symbol, data)
+
+    def openChannel(self):
+        self._logger.info("Going to open the communication channel...")
+        self.channel.open()
+        self._logger.info("Communication channel opened.")
+
+    def closeChannel(self):
+        self._logger.info("Going to close the communication channel...")
+        self.channel.close()
+        self._logger.info("Communication channel close.")
