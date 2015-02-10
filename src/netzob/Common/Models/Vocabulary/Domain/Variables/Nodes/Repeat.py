b6c187ffdcadee08c70071aa51d588f35f72847e
Georges Bossert <georges.bossert@supelec.fr>
2015-02-10 23:29:45 +0100

    Fix small issue in Repeat domain

--- src/netzob/Common/Models/Vocabulary/Domain/Variables/Nodes/Repeat.py
+++ src/netzob/Common/Models/Vocabulary/Domain/Variables/Nodes/Repeat.py
@@ -1,57 +1,53 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-#+---------------------------------------------------------------------------+
-#|          01001110 01100101 01110100 01111010 01101111 01100010            |
-#|                                                                           |
-#|               Netzob : Inferring communication protocols                  |
-#+---------------------------------------------------------------------------+
-#| Copyright (C) 2011-2014 Georges Bossert and Frédéric Guihéry              |
-#| This program is free software: you can redistribute it and/or modify      |
-#| it under the terms of the GNU General Public License as published by      |
-#| the Free Software Foundation, either version 3 of the License, or         |
-#| (at your option) any later version.                                       |
-#|                                                                           |
-#| This program is distributed in the hope that it will be useful,           |
-#| but WITHOUT ANY WARRANTY; without even the implied warranty of            |
-#| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
-#| GNU General Public License for more details.                              |
-#|                                                                           |
-#| You should have received a copy of the GNU General Public License         |
-#| along with this program. If not, see <http://www.gnu.org/licenses/>.      |
-#+---------------------------------------------------------------------------+
-#| @url      : http://www.netzob.org                                         |
-#| @contact  : contact@netzob.org                                            |
-#| @sponsors : Amossys, http://www.amossys.fr                                |
-#|             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
-#+---------------------------------------------------------------------------+
+# +---------------------------------------------------------------------------+
+# |          01001110 01100101 01110100 01111010 01101111 01100010            |
+# |                                                                           |
+# |               Netzob : Inferring communication protocols                  |
+# +---------------------------------------------------------------------------+
+# | Copyright (C) 2011-2014 Georges Bossert and Frédéric Guihéry              |
+# | This program is free software: you can redistribute it and/or modify      |
+# | it under the terms of the GNU General Public License as published by      |
+# | the Free Software Foundation, either version 3 of the License, or         |
+# | (at your option) any later version.                                       |
+# |                                                                           |
+# | This program is distributed in the hope that it will be useful,           |
+# | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+# | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              |
+# | GNU General Public License for more details.                              |
+# |                                                                           |
+# | You should have received a copy of the GNU General Public License         |
+# | along with this program. If not, see <http://www.gnu.org/licenses/>.      |
+# +---------------------------------------------------------------------------+
+# | @url      : http://www.netzob.org                                         |
+# | @contact  : contact@netzob.org                                            |
+# | @sponsors : Amossys, http://www.amossys.fr                                |
+# |             Supélec, http://www.rennes.supelec.fr/ren/rd/cidre/           |
+# +---------------------------------------------------------------------------+
 
-#+---------------------------------------------------------------------------+
-#| File contributors :                                                       |
-#|       - Georges Bossert <georges.bossert (a) supelec.fr>                  |
-#|       - Frédéric Guihéry <frederic.guihery (a) amossys.fr>                |
-#+---------------------------------------------------------------------------+
+# +---------------------------------------------------------------------------+
+# | File contributors :                                                       |
+# |       - Georges Bossert <georges.bossert (a) supelec.fr>                  |
+# |       - Frédéric Guihéry <frederic.guihery (a) amossys.fr>                |
+# +---------------------------------------------------------------------------+
 
-#+---------------------------------------------------------------------------+
-#| Standard library imports                                                  |
-#+---------------------------------------------------------------------------+
+# +---------------------------------------------------------------------------+
+# | Standard library imports                                                  |
+# +---------------------------------------------------------------------------+
 import random
 from bitarray import bitarray
 
-#+---------------------------------------------------------------------------+
-#| Related third party imports                                               |
-#+---------------------------------------------------------------------------+
+# +---------------------------------------------------------------------------+
+# | Related third party imports                                               |
+# +---------------------------------------------------------------------------+
 
-#+---------------------------------------------------------------------------+
-#| Local application imports                                                 |
-#+---------------------------------------------------------------------------+
+# +---------------------------------------------------------------------------+
+# | Local application imports                                                 |
+# +---------------------------------------------------------------------------+
 from netzob.Common.Utils.Decorators import typeCheck, NetzobLogger
 from netzob.Common.Models.Vocabulary.Domain.Variables.Nodes.AbstractVariableNode import AbstractVariableNode
 from netzob.Common.Models.Vocabulary.Domain.Parser.ParsingPath import ParsingPath
 from netzob.Common.Models.Vocabulary.Domain.Specializer.SpecializingPath import SpecializingPath
-from netzob.Common.Models.Types.TypeConverter import TypeConverter
-from netzob.Common.Models.Types.Raw import Raw
-from netzob.Common.Models.Types.BitArray import BitArray
-
 
 
 @NetzobLogger
@@ -80,8 +76,8 @@ class Repeat(AbstractVariableNode):
     >>> mp = MessageParser()
     >>> print mp.parseMessage(msg1, s)
     [bitarray('011011100110010101110100011110100110111101100010001110110111101001101111011000100111100100111011011011100110010101110100011110100110111101100010'), bitarray('01111010011011110110001001111001')]
-    
-    
+
+
     Let's illustrate the specialization of a repeat:
 
     >>> from netzob.all import *
@@ -112,10 +108,10 @@ class Repeat(AbstractVariableNode):
     def __init__(self, child, nbRepeat, delimitor=None):
         super(Repeat, self).__init__(self.__class__.__name__, [child])
         self.nbRepeat = nbRepeat
-        self.delimitor = delimitor        
+        self.delimitor = delimitor
 
     @typeCheck(ParsingPath)
-    def parse(self, parsingPath):
+    def parse(self, parsingPath, carnivorous=False):
         """Parse the content with the definition domain of the Repeat
         """
 
@@ -127,9 +123,8 @@ class Repeat(AbstractVariableNode):
 
         # remove any data assigned to this variable
         parsingPath.removeAssignedDataToVariable(self)
-        
-        self._logger.debug("Parse '{0}' as {1} with parser path '{2}'".format(dataToParse, self, parsingPath))
 
+        self._logger.debug("Parse '{0}' as {1} with parser path '{2}'".format(dataToParse, self, parsingPath))
 
         results = []
         # we try to parse according to the various different number of repetitions
@@ -141,21 +136,21 @@ class Repeat(AbstractVariableNode):
             for i in xrange(i_repeat):
                 tmp_result = []
                 for newParsingPath in newParsingPaths:
-                    childParsingPaths = self.children[0].parse(newParsingPath)
+                    childParsingPaths = self.children[0].parse(newParsingPath, carnivorous=carnivorous)
                     for childParsingPath in childParsingPaths:
                         if childParsingPath.isDataAvailableForVariable(self):
                             newResult = childParsingPath.getDataAssignedToVariable(self).copy()
-                            newResult+= childParsingPath.getDataAssignedToVariable(self.children[0])                            
+                            newResult += childParsingPath.getDataAssignedToVariable(self.children[0])
                         else:
-                            newResult = childParsingPath.getDataAssignedToVariable(self.children[0])                            
-            
+                            newResult = childParsingPath.getDataAssignedToVariable(self.children[0])
+
                         childParsingPath.addResult(self, newResult)
                         childParsingPath.assignDataToVariable(dataToParse.copy()[len(newResult):], self.children[0])
 
                         if self.delimitor is not None:
-                            if i<i_repeat-1:
+                            if i < i_repeat - 1:
                                 # check the delimitor is available
-                                toParse = childParsingPath.getDataAssignedToVariable(self.children[0]).copy()                                
+                                toParse = childParsingPath.getDataAssignedToVariable(self.children[0]).copy()
                                 if toParse[:len(self.delimitor)] == self.delimitor:
                                     newResult = childParsingPath.getDataAssignedToVariable(self).copy() + self.delimitor
                                     childParsingPath.addResult(self, newResult)
@@ -163,14 +158,14 @@ class Repeat(AbstractVariableNode):
                                     tmp_result.append(childParsingPath)
                             else:
                                 tmp_result.append(childParsingPath)
-                                
+
                         else:
                             tmp_result.append(childParsingPath)
                 newParsingPaths = tmp_result
 
             for newParsingPath in newParsingPaths:
                 results.append(newParsingPath)
-        
+
         return results
 
     @typeCheck(SpecializingPath)
@@ -185,25 +180,23 @@ class Repeat(AbstractVariableNode):
 
         for i_repeat in xrange(self.nbRepeat[0], self.nbRepeat[1]):
             newSpecializingPaths = [originalSpecializingPath.duplicate()]
-            
-            for i in xrange(i_repeat):                
-                childSpecializingPaths = []            
+
+            for i in xrange(i_repeat):
+                childSpecializingPaths = []
                 for newSpecializingPath in newSpecializingPaths:
                     for path in self.children[0].specialize(newSpecializingPath):
                         if path.isDataAvailableForVariable(self):
                             newResult = path.getDataAssignedToVariable(self).copy()
                             if self.delimitor is not None:
                                 newResult += self.delimitor
-                            newResult+= path.getDataAssignedToVariable(self.children[0])
+                            newResult += path.getDataAssignedToVariable(self.children[0])
                         else:
                             newResult = path.getDataAssignedToVariable(self.children[0])
                         path.addResult(self, newResult)
                         childSpecializingPaths.append(path)
-                
+
                 newSpecializingPaths = childSpecializingPaths
             specializingPaths.extend(newSpecializingPaths)
-            
-            
 
         # lets shuffle this ( :) ) >>> by default we only consider the first valid parsing path.
         random.shuffle(specializingPaths)
@@ -217,11 +210,11 @@ class Repeat(AbstractVariableNode):
     @nbRepeat.setter
     def nbRepeat(self, nbRepeat):
         if nbRepeat is None:
-            raise Exception("NB Repeat cannot be None")        
+            raise Exception("NB Repeat cannot be None")
         MAX_REPEAT = 1000
 
         if isinstance(nbRepeat, int):
-            nbRepeat = (nbRepeat, nbRepeat+1)
+            nbRepeat = (nbRepeat, nbRepeat + 1)
 
         if isinstance(nbRepeat, tuple):
             minNbRepeat, maxNbRepeat = nbRepeat
@@ -251,6 +244,3 @@ class Repeat(AbstractVariableNode):
     @typeCheck(bitarray)
     def delimitor(self, delimitor):
         self.__delimitor = delimitor
-    
-            
-        
