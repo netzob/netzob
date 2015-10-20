dbc70cc30562b24fe3d12cd2b1db4115186ea61f
Georges Bossert <gbossert@miskin.fr>
2015-10-20 09:02:24 +0200

    Excluding 'all.py' files from autodoc

--- doc/documentation/source/conf.py
+++ doc/documentation/source/conf.py
@@ -71,7 +71,7 @@ release = '1.0~git'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ["all.py"]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
