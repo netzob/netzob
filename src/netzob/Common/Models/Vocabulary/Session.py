b5136491c0a7e2db5257b2f70d2fa64e39ce0599
Georges Bossert <gbossert@miskin.fr>
2016-03-31 21:07:41 +0200

    Adapt doctest in Session.py to support different timezones

--- src/netzob/Common/Models/Vocabulary/Session.py
+++ src/netzob/Common/Models/Vocabulary/Session.py
@@ -59,12 +59,12 @@ class Session(object):
     >>> msg2 = RawMessage("SYN", source="A", destination="B", date=time.mktime(time.strptime("9 Aug 13 10:45:01", "%d %b %y %H:%M:%S")))
     >>> msg3 = RawMessage("SYN/ACK", source="B", destination="A", date=time.mktime(time.strptime("9 Aug 13 10:45:03", "%d %b %y %H:%M:%S")))
     >>> session = Session([msg1, msg2, msg3])
-    >>> print session.messages.values()[0]
-    [0;32m[1376037901.0 [0;m[1;32mA[1;m[0;32m->[0;m[1;32mB[1;m[0;32m][0;m 'SYN'
-    >>> print session.messages.values()[1]
-    [0;32m[1376037903.0 [0;m[1;32mB[1;m[0;32m->[0;m[1;32mA[1;m[0;32m][0;m 'SYN/ACK'
-    >>> print session.messages.values()[2]
-    [0;32m[1376037905.0 [0;m[1;32mA[1;m[0;32m->[0;m[1;32mB[1;m[0;32m][0;m 'ACK'
+    >>> print session.messages.values()[0].data
+    SYN
+    >>> print session.messages.values()[1].data
+    SYN/ACK
+    >>> print session.messages.values()[2].data
+    ACK
 
     """
 
