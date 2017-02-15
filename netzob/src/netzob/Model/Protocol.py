2c248d9a139bdcc907848c9160f2ee6fe509b5b1
Frédéric Guihéry <frederic.guihery@amossys.fr>
2017-02-15 14:55:22 +0100

    Allow to load ZDL format and automata files through netzob.Protocol class
    -- Carried out under contract n°1100036530 for ANSSI --

--- netzob/src/netzob/Model/Protocol.py
+++ netzob/src/netzob/Model/Protocol.py
@@ -34,6 +34,7 @@
 #+---------------------------------------------------------------------------+
 #| Standard library imports                                                  |
 #+---------------------------------------------------------------------------+
+import imp
 
 #+---------------------------------------------------------------------------+
 #| Related third party imports                                               |
@@ -43,14 +44,13 @@
 #| Local application imports                                                 |
 #+---------------------------------------------------------------------------+
 from netzob.Common.Utils.Decorators import typeCheck
+from netzob.Model.Grammar.Automata import Automata
 
 
 class Protocol(object):
     """The model of a protocol.
 
     It regroups both a vocabulary and a grammar.
-
-    To create a protocol, you only need its name.
 
     >>> icmp = Protocol("ICMP")
     >>> icmp.name
@@ -61,13 +61,45 @@ class Protocol(object):
 
     """
 
-    def __init__(self, name=None):
+    def __init__(self, name=None, format_zdl=None, automata_zdl=None):
         """
         :keyword name: the name of the protocol
         :type name: an :class:`str`
 
         """
         self.__name = name
+        self.__symbols = {}
+        self.__automata = None
+
+        if format_zdl is not None:
+            self._initializeSymbols(format_zdl)
+
+        if automata_zdl is not None:
+            self._initializeAutomata(automata_zdl)
+
+    def _initializeSymbols(self, path, modLoaded=None):
+        """Parse a dictionary of symbols from a ZDL file.
+        """
+
+        if modLoaded is None:
+            # Load module from source ZDL file
+            modLoaded = imp.load_source("format", path)
+
+        # Built dictionary of symbols
+        for s in modLoaded.symbols:
+            if s.name in self.symbols:
+                raise Exception("Multiple symbols have the same name: {}. Symbol name should be unique.".format(s.name))
+            self.symbols[s.name] = s
+
+    def _initializeAutomata(self, path):
+        """Parse an automata from a ZDL file.
+        """
+
+        # Load module from source ZDL file
+        mod = imp.load_source("automata", path)
+
+        # Retrieve automata
+        self.automata = mod.automata
 
     @property
     def name(self):
@@ -84,3 +116,21 @@ class Protocol(object):
     @typeCheck(str)
     def name(self, name):
         self.__name = name
+
+    @property
+    def symbols(self):
+        return self.__symbols
+
+    @symbols.setter
+    @typeCheck(dict)
+    def symbols(self, symbols):
+        self.__symbols = symbols
+
+    @property
+    def automata(self):
+        return self.__automata
+
+    @automata.setter
+    @typeCheck(Automata)
+    def automata(self, automata):
+        self.__automata = automata
