4a272effa1ad827afb9517c5800cf7cd352566a4
Frédéric Guihéry <frederic.guihery@amossys.fr>
2017-03-28 16:32:36 +0200

    Add max write mechanism (ANS015 CCTP req.90). By default this mechanism is deactivated, so no regression of existing use of Netzob.
    -- Carried out under contract n°1100036530 for ANSSI --

--- netzob/src/netzob/Simulator/Channels/AbstractChannel.py
+++ netzob/src/netzob/Simulator/Channels/AbstractChannel.py
@@ -98,17 +98,6 @@ class AbstractChannel(object, metaclass=abc.ABCMeta):
         """
         self.close()
 
-    def __enter__(self):
-        """Enter the runtime channel context.
-        """
-        self.open()
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        """Exit the runtime channel context.
-        """
-        self.close()
-
     # Static methods used to retrieve local network interface
     # and local IP according to a remote IP
 
