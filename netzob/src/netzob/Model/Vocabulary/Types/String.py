2f4d76030f70944c0d4d989376da6eca8d7bbf67
Frédéric Guihéry <frederic.guihery@amossys.fr>
2017-06-01 17:52:35 +0200

    model(String): change 'eof' to 'eos' for String param regarding permitted ending characters

--- netzob/src/netzob/Model/Vocabulary/Types/String.py
+++ netzob/src/netzob/Model/Vocabulary/Types/String.py
@@ -65,7 +65,7 @@ class String(AbstractType):
     :param nbChars: The amount of permitted String characters.
     :param encoding: The encoding of the string, such as 'ascii' or
                     'utf-8'. Default value is 'utf-8'.
-    :param eof: A list defining the potential terminal characters for
+    :param eos: A list defining the potential terminal characters for
                 the string, with either specific constants or pointers
                 to other fields containing the permitted terminal
                 values. Default value is an empty list, meaning there
@@ -76,7 +76,7 @@ class String(AbstractType):
     :type value: :class:`bitarray.bitarray`, optional
     :type nbChars: an :class:`int` or a tuple with the min and the max size specified as :class:`int`, optional
     :type encoding: :class:`str`, optional
-    :type eof: a :class:`list` of :class:`AbstractType <netzob.Model.Vocabulary.Types.AbstractType>` or a :class:`list` of :class:`Field <netzob.Model.Vocabulary.Field>`, optional
+    :type eos: a :class:`list` of :class:`AbstractType <netzob.Model.Vocabulary.Types.AbstractType>` or a :class:`list` of :class:`Field <netzob.Model.Vocabulary.Field>`, optional
 
 
     Supported encodings are available on the Python reference documentation:
@@ -127,14 +127,14 @@ class String(AbstractType):
 
     .. todo::
 
-        # >>> f_eof    = Field(String('\t'))
-        # >>> f_string = Field(String(eof=[String('\n'), Raw(b'\x00'), f_eof]))
+        # >>> f_eos    = Field(String('\t'))
+        # >>> f_string = Field(String(eos=[String('\n'), Raw(b'\x00'), f_eos]))
 
-    The ``eof`` attribute specifies a list of values that is used as
+    The ``eos`` attribute specifies a list of values that is used as
     potential terminal characters. Terminal characters can either be
     defined as constants (such as ``String('\n')`` and ``Raw('\x00')``
     in the previous example) or defined as specific fields (such as
-    ``f_eof`` in the previous example).
+    ``f_eos`` in the previous example).
 
 
     **Examples of String Internal Attributes Access**
