d51793020b68d9b2d9a7f2b23f35caa803608bc7
Remy Delion <remy.delion@amossys.fr>
2017-10-03 17:33:12 +0200

    Add RawEthernetChannel.useEthernetHeader property to define if the channel has to generate the Ethernet header (True) or if this header is already included in the data to send (False).

--- netzob/src/netzob/Simulator/Channels/RawEthernetChannel.py
+++ netzob/src/netzob/Simulator/Channels/RawEthernetChannel.py
@@ -101,15 +101,17 @@ class RawEthernetChannel(AbstractChannel):
     def __init__(self,
                  remoteMac,
                  localMac,
+                 interface,
                  timeout=AbstractChannel.DEFAULT_TIMEOUT):
         super(RawEthernetChannel, self).__init__(timeout=timeout)
         self.remoteMac = remoteMac
         self.localMac = localMac
+        self.__interface = interface
         self.__socket = None
 
-        self.initHeader()
+        self.__useEthernetHeader = False
 
-        #TODO: retrieve the network interface from the local MAC
+        self.initHeader()
 
     @staticmethod
     def getBuilder():
@@ -220,8 +222,11 @@ class RawEthernetChannel(AbstractChannel):
         if self.__socket is None:
             raise Exception("socket is not available")
 
-        self.header_presets["eth.payload"] = data
-        packet = self.header.specialize(presets=self.header_presets)
+        if self.__useEthernetHeader:
+            self.header_presets["eth.payload"] = data
+            packet = self.header.specialize(presets=self.header_presets)
+        else:
+            packet = data
         len_data = self.__socket.sendto(packet, (self.interface,
                                                  RawEthernetChannel.ETH_P_ALL))
         return len_data
@@ -298,6 +303,20 @@ class RawEthernetChannel(AbstractChannel):
         """
         return self.__interface
 
+    @property
+    def useEthernetHeader(self):
+        """Defines if the channel has to prepare the Ethernet header fields
+        before sending a data.
+
+        :type: :class:`bool`
+        """
+        return self.__useEthernetHeader
+
+    @useEthernetHeader.setter
+    @typeCheck(bool)
+    def useEthernetHeader(self, useEthernetHeader):
+        self.__useEthernetHeader = useEthernetHeader
+
 
 class RawEthernetChannelBuilder(ChannelBuilder):
     """
@@ -321,3 +340,6 @@ class RawEthernetChannelBuilder(ChannelBuilder):
 
     def set_dst_addr(self, value):
         self.attrs['remoteMac'] = value
+
+    def set_interface(self, value):
+        self.attrs['interface'] = value
