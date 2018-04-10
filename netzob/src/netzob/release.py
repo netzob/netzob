3e8a66f1b7a6193cac394b81832b0f40636543a4
Remy Delion <remy.delion@amossys.fr>
2018-04-10 14:39:25 +0200

    Version bump: 1.1.6-

--- netzob/src/netzob/release.py
+++ netzob/src/netzob/release.py
@@ -42,7 +42,7 @@ appname = name
 #|    Development version has version number increased and is
 #|    postfixed by .dev0
 #+---------------------------------------------------------------------------+
-version = "1.1.5-cottontail"
+version = "1.1.6-cottontail"
 versionName = "SilkyFur"
 
 #+---------------------------------------------------------------------------+
