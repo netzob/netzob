52754d212316d6e8ec728c80b9645d1c8e3dc6bd
Remy Delion <remy.delion@amossys.fr>
2018-10-17 18:31:52 +0200

    Version bump: 1.2.20-

--- netzob/src/netzob/release.py
+++ netzob/src/netzob/release.py
@@ -42,8 +42,8 @@ appname = name
 #|    Development version has version number increased and is
 #|    postfixed by .dev0
 #+---------------------------------------------------------------------------+
-version = "1.1.19-cottontail"
-versionName = "SpittingLama"
+version = "1.1.20-cottontail"
+versionName = "SpittingFly"
 
 #+---------------------------------------------------------------------------+
 #| Copyright mentions
