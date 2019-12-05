8063b5064b9753c88174bdae0fa9cc895b0c6881
Alexandre Pigné <alexandre.pigne@amossys.fr>
2019-12-05 09:54:45 +0100

    Remove unnecessary call to -privcap

--- netzob/src/netzob/Simulator/AbstractChannel.py
+++ netzob/src/netzob/Simulator/AbstractChannel.py
@@ -885,10 +885,8 @@ class NetUtils(object):
         else:
             cmd = "tc qdisc replace dev {} root netem rate {}".format(
                 localInterface, rate * 8)  # in bits per second
-        env = os.environ
-        cmd = "cottontail-privcap TC {}".format(cmd)
         cmd = shlex.split(cmd)
-        p = subprocess.Popen(cmd, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (stdoutData, stderrData) = p.communicate()
         if p.returncode != 0 and rate is not None:
             # if rate is None no tc rule was previously defined on th interface,
