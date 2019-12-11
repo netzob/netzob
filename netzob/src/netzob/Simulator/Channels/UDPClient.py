0b0d2ec51522329ee94af47d17f4e401815e7e49
Remy Delion <remy.delion@amossys.fr>
2019-12-11 14:31:05 +0100

    tc log correction

--- netzob/src/netzob/Simulator/Channels/UDPClient.py
+++ netzob/src/netzob/Simulator/Channels/UDPClient.py
@@ -297,8 +297,8 @@ class UDPClient(AbstractChannel):
     def unset_rate(self):
         """This method clears the transmission rate.
         """
+        localInterface = NetUtils.getLocalInterface(self.localIP)
         if self._rate is not None:
-            localInterface = NetUtils.getLocalInterface(self.localIP)
             NetUtils.set_rate(localInterface, None)
             self._rate = None
             self._logger.info("Network rate limitation removed on {} interface".format(localInterface))
